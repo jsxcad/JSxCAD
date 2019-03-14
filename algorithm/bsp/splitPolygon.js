1dd75d28bfaa590450a5aa50b285e3b647b99b12
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-03-14 22:36:10 +0900

    Compare.

--- algorithm/bsp/splitPolygon.js
+++ algorithm/bsp/splitPolygon.js
@@ -1,5 +1,5 @@
 import { toPlane } from '@jsxcad/math-poly3';
-import { canonicalize, dot, lerp, subtract } from '@jsxcad/math-vec3';
+import { dot, lerp, subtract } from '@jsxcad/math-vec3';
 
 const EPSILON = 1e-5;
 
@@ -11,7 +11,6 @@ const SPANNING = 3; // Both front and back.
 const W = 3;
 
 export const splitPolygon = (plane, coplanarFront, coplanarBack, front, back, polygon) => {
-  if (polygon.length !== 3) throw Error('impossible');
   // Classify each point as well as the entire polygon into one of the above
   // four classes.
   let polygonType = 0;
@@ -61,29 +60,18 @@ export const splitPolygon = (plane, coplanarFront, coplanarBack, front, back, po
         if ((startType | endType) === SPANNING) {
         // Compute the point that touches the splitting plane.
           let t = (plane[W] - dot(plane, startPoint)) / dot(plane, subtract(endPoint, startPoint));
-          let spanPoint = canonicalize(lerp(t, startPoint, endPoint));
-          // let spanPoint = lerp(t, startPoint, endPoint);
+          let spanPoint = lerp(t, startPoint, endPoint);
           frontPoints.push(spanPoint);
           backPoints.push(spanPoint);
         }
       }
-      if (frontPoints.length == 3) {
+      if (frontPoints.length >= 3) {
       // Add the polygon that sticks out the front of the plane.
         front.push(frontPoints);
-      } else if (frontPoints.length == 4) {
-        front.push([frontPoints[0], frontPoints[1], frontPoints[3]]);
-        front.push([frontPoints[3], frontPoints[1], frontPoints[2]]);
-      } else {
-        throw Error('impossible');
       }
-      if (backPoints.length == 3) {
+      if (backPoints.length >= 3) {
       // Add the polygon that sticks out the back of the plane.
         back.push(backPoints);
-      } else if (backPoints.length == 4) {
-        back.push([backPoints[0], backPoints[1], backPoints[3]]);
-        back.push([backPoints[3], backPoints[1], backPoints[2]]);
-      } else {
-        throw Error('impossible');
       }
       break;
     }
