b814a0bb2e980fa55131cf5de7dbfdd3e42970c6
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-03-28 23:19:24 +0900

    More cleaner.

--- example/v1/cutCubes.js
+++ example/v1/cutCubes.js
@@ -7,8 +7,8 @@ export const main = () => {
   const input = toTriangles({}, union(cube(30).translate([0, 0, 0]),
                                       cube(30).translate([5, 5, -1]),
                                       cube(30).translate([-5, -5, -1])
-                                     ).toPaths({}));
+  ).toPaths({}));
   const paths = cutTrianglesByPlane(fromPoints([0, 0, 0], [1, 0, 0], [0, 1, 0]), input);
   const polygons = unionOfZ0Polygons(...paths.map(path => [path]));
   writePdf({ path: 'tmp/cutCubes.pdf' }, polygons);
-}
+};
