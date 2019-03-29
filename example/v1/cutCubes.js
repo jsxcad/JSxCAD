953d0e1e5b43cfffa96770af2325813667b7d769
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-03-29 14:21:16 +0900

    Lint.

--- example/v1/cutCubes.js
+++ example/v1/cutCubes.js
@@ -4,7 +4,7 @@ export const main = () => {
   const paths = union(cube(30).translate([0, 0, 0]),
                       cube(30).translate([5, 5, -1]),
                       cube(30).translate([-5, -5, -1]))
-                  .crossSection();
+      .crossSection();
 
   writePdf({ path: 'tmp/cutCubes.pdf' }, paths);
 };
