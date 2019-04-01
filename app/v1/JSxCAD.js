cdbd16ecfd45dc012e1a1fd56043ab4b757c3b66
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-04-02 00:11:21 +0900

    Update app example with trivial multiple windows

--- app/v1/JSxCAD.js
+++ app/v1/JSxCAD.js
@@ -1,11 +1,12 @@
 import * as api from '@jsxcad/api-v1';
 import { trianglesToThreejsDatasets } from '@jsxcad/algorithm-threejs';
-import { readFileSync, watchFile, writeFileSync } from '@jsxcad/sys';
+import { readFileSync, watchFile, watchFileCreation, writeFileSync } from '@jsxcad/sys';
 
 export {
   api,
   readFileSync,
   trianglesToThreejsDatasets,
   watchFile,
+  watchFileCreation,
   writeFileSync
 };
