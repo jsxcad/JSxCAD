e6974ef98847fc5461bb6e010a858b9c8f8247a1
BarbourSmith <BarbourSmith@users.noreply.github.com>
2019-04-03 18:06:39 +0700

    stash

---
+++ api/v1/hull.js
@@ -0,0 +1,8 @@
+import { buildConvexHull } from '@jsxcad/algorithm-shape';
+import { toPoints } from '@jsxcad/algorithm-paths';
+
+const hull = (...geometries) => {
+    console.log("hull code ran");
+}
+
+module.exports = hull;
