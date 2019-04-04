1ac40e2042d2b27dbf4731e814a07c5c17c946b6
BarbourSmith <BarbourSmith@users.noreply.github.com>
2019-04-04 15:44:14 +0700

    Add a test

---
+++ example/v1/sphereCubeHull.js
@@ -0,0 +1,9 @@
+import { cube, sphere, writeStl, hull } from '@jsxcad/api-v1';
+
+export function main () {
+  let x = hull(sphere(),
+                cube({ size: 1, center: true }).translate([0.5, 0.5, 3.0]),
+                cube({ size: 1, center: true }).translate([0.5, -1, 0]));
+
+  writeStl({ path: 'tmp/sphereCubeHull.stl' }, x);
+}
