e8745ba2530a9a93e606ab5c1f04c12100094d3a
BarbourSmith <BarbourSmith@users.noreply.github.com>
2019-04-04 11:39:02 +0700

    Ooops forgot to run lintfix :frowning:

--- api/v1/hull.js
+++ api/v1/hull.js
@@ -1,14 +1,11 @@
 import { buildConvexHull } from '@jsxcad/algorithm-shape';
 import { CSG } from './CSG';
-import { toPoints } from '@jsxcad/algorithm-paths';
-
 
 export const hull = (...geometries) => {
-    
-    var allPoints = [];
-    for (const geometry of geometries) {
-        allPoints = allPoints.concat(geometry.toPoints());
-    }
-    
-    return CSG.fromPolygons(buildConvexHull({}, allPoints))
-}
+  var allPoints = [];
+  for (const geometry of geometries) {
+    allPoints = allPoints.concat(geometry.toPoints());
+  }
+
+  return CSG.fromPolygons(buildConvexHull({}, allPoints));
+};
