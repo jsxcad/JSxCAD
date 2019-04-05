21bd74fea714d61a4c8770ab5c315d4226374eaa
BarbourSmith <BarbourSmith@users.noreply.github.com>
2019-04-05 15:04:24 +0700

    clean up

--- sys/writeFileSyncBrowser.js
+++ sys/writeFileSyncBrowser.js
@@ -3,17 +3,16 @@ const { getFile } = require('./files');
 const writeFileSync = (path, data, options) => {
   const file = getFile(path);
   const { translator } = options;
-  
+
   file.data = data;
-  
-  if(translator){
+
+  if (translator) {
     file.translator = translator;
   }
-  
+
   for (const watcher of file.watchers) {
     watcher(file);
   }
-  
 };
 
 module.exports.writeFileSync = writeFileSync;
