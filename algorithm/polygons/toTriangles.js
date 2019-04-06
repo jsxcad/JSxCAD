36b412f72bb04851d2a157e552bc0d32a415d601
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-04-06 20:19:30 +0900

    Remove logging.

--- algorithm/polygons/toTriangles.js
+++ algorithm/polygons/toTriangles.js
@@ -2,7 +2,6 @@ import { blessAsTriangles } from './blessAsTriangles';
 import { isTriangle } from './isTriangle';
 
 export const toTriangles = (options = {}, paths) => {
-  console.log(paths);
   if (paths.isTriangles) {
     return paths;
   }
