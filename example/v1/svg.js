a586f5d2d1be06f3e437cabdfe9ab1aac12207ed
BarbourSmith <BarbourSmith@users.noreply.github.com>
2019-04-08 11:04:31 +0700

    Close paths and add a test

--- example/v1/svg.js
+++ example/v1/svg.js
@@ -1,3 +1,12 @@
-import { sphere, writeSvg } from '@jsxcad/api-v1';
+import { difference, sphere, writeSvg } from '@jsxcad/api-v1';
+import { cutTrianglesByPlane, toTriangles } from '@jsxcad/algorithm-polygons';
+import { fromPoints } from '@jsxcad/math-plane';
 
-export const main = () => writeSvg({ path: 'tmp/cube.svg' }, sphere(10));
+export const main = () => {
+  const z = 0;
+  const shape = difference(sphere(30), sphere(15));
+  const triangles = toTriangles({}, shape.toSolid());
+  const paths = cutTrianglesByPlane(fromPoints([0, 0, z], [1, 0, z], [0, 1, z]), triangles);
+  writeSvg({ path: 'tmp/cutSpheres.svg' }, paths);
+  writeSvg({ path: 'tmp/sphere.svg' }, sphere(10));
+};
