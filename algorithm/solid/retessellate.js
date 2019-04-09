029a04e8f03717486c5e44db8da8fb6ed9b335f8
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-04-10 00:29:37 +0900

    Add algorithm/solid with retessellate.

---
+++ algorithm/solid/retessellate.js
@@ -0,0 +1,33 @@
+import { makeConvex, transform } from '@jsxcad/algorithm-polygons';
+import { toPlane } from '@jsxcad/math-poly3';
+import { toXYPlaneTransforms } from '@jsxcad/math-plane';
+import { union } from '@jsxcad/algorithm-z0polygons';
+
+export const retessellate = ({ emitSurface }, solid) => {
+  const coplanarGroups = new Map();
+
+  for (const polygon of solid) {
+    const plane = toPlane(polygon);
+    const key = JSON.stringify(plane);
+    const groups = coplanarGroups.get(key);
+    if (groups === undefined) {
+      coplanarGroups.set(key, [polygon]);
+    } else {
+      groups.push(polygon);
+    }
+  }
+
+  const retessellated = [];
+  const surfaces = [];
+
+  for (const group of coplanarGroups.values()) {
+    const [to, from] = toXYPlaneTransforms(toPlane(group[0]));
+    let surface = makeConvex({}, union(...transform(to, group).map(polygon => [polygon])));
+    if (emitSurface) {
+      emitSurface(surface);
+    }
+    retessellated.push(...transform(from, surface));
+  }
+
+  return retessellated;
+};
