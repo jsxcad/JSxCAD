464fa009eab356908e3e9dbee80e60c49c14cdd8
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-04-12 02:15:07 +0900

    Clean up lint.

--- example/v1/hanyi.js
+++ example/v1/hanyi.js
@@ -3,8 +3,8 @@ import { loadFont, text, writePdf, writeStl } from '@jsxcad/api-v1';
 const hanyi = loadFont({ path: './hanyi/HanyiSentyTang.ttf' });
 
 export const main = ({ string = '福' }) => {
-  const surface = text({ font: hanyi, curveSegments: 4 }, string);
+  const surface = text({ font: hanyi, curveSegments: 32 }, string);
   const extrusion = surface.extrude({ height: 5 });
   writePdf({ path: 'tmp/hanyi.pdf' }, surface);
-  writeStl({ path: 'tmp/hanyi.stl' }, surface.extrude({ height: 5 }));
+  writeStl({ path: 'tmp/hanyi.stl' }, extrusion);
 };
