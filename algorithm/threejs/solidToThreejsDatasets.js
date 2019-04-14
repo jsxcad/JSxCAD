09fbacb53bd2225883536691b54e3d15c84354f2
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-04-14 13:43:34 +0900

    Update threejs rendering for new solid representation.

--- algorithm/threejs/solidToThreejsDatasets.js
+++ algorithm/threejs/solidToThreejsDatasets.js
@@ -1,5 +1,6 @@
 import { toTriangles } from '@jsxcad/algorithm-polygons';
+import { makeSurfacesConvex, toPolygons } from '@jsxcad/algorithm-solid';
 import { trianglesToThreejsDatasets } from './trianglesToThreejsDatasets';
 
 export const solidToThreejsDatasets = (options = {}, ...solids) =>
-  trianglesToThreejsDatasets(options, ...solids.map(solid => toTriangles({}, solid)));
+  trianglesToThreejsDatasets(options, ...solids.map(solid => toTriangles({}, toPolygons({}, makeSurfacesConvex({}, solid)))));
