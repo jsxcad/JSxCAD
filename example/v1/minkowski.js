32195b8dc8146248e14c8e7fbbc4e1e9e028f90c
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-04-15 14:49:05 +0900

    Clean up more.

--- example/v1/minkowski.js
+++ example/v1/minkowski.js
@@ -1,5 +1,4 @@
 import { cube, sphere, minkowski, writeStl } from '@jsxcad/api-v1';
-import { fromPolygons } from '@jsxcad/algorithm-solid';
 
 export const main = () => {
   writeStl({ path: 'tmp/minkowski.stl' },
