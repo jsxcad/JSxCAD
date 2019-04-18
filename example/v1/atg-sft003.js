2f0fd866856cdb1a69754006bf44421e2ccacaab
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-04-18 21:18:54 +0900

    Add missing await.

--- example/v1/atg-sft003.js
+++ example/v1/atg-sft003.js
@@ -2,5 +2,5 @@ import { readDst, writePdf } from '@jsxcad/api-v1';
 
 export const main = async () => {
   const paths = await readDst({ path: 'atg-sft003.dst' });
-  writePdf({ path: 'tmp/atg-sft003.pdf' }, paths);
+  await writePdf({ path: 'tmp/atg-sft003.pdf' }, paths);
 };
