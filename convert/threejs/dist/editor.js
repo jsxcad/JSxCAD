f9f73ed7cfbdaeef36f66999c0a27f3cc69d9d44
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-05-01 22:11:23 +0900

    Missing files.

---
+++ convert/threejs/dist/editor.js
@@ -0,0 +1,52 @@
+let editor;
+
+const runScript = () => {
+  // Evaluate.
+  const code = new Function(`{ ${Object.keys(api).join(', ')} }`, editor.getDoc().getValue('\n'));
+  const result = code(api);
+  // Handle a return value as an implicit write.
+  if (result) {
+    api.writePaths({ path: 'default' }, result);
+  }
+  nextPage();
+}
+
+const setupDocument = () => {
+  editor = CodeMirror((domElement) => {
+                        domElement.id = 'editor';
+                        addPage(domElement);
+                      },
+                      {
+                        autoRefresh: true,
+                        mode: "javascript",
+                        theme: "default",
+                        fullScreen: true,
+                        lineNumbers: true,
+                        gutter: true,
+                        lineWrapping: true,
+                        extraKeys: {
+                          'Shift-Enter': runScript,
+                        },
+                        value: (typeof initialScript === 'undefined') ? '' : initialScript
+                      });
+  document.addEventListener("keydown",
+                            e => {
+                              switch (e.keyCode) {
+                                case 34:
+                                  // Page Down.
+                                  nextPage();
+                                  e.preventDefault();
+                                  e.stopPropagation();
+                                  break;
+                                case 33: 
+                                  // Page Up.
+                                  lastPage();
+                                  e.preventDefault();
+                                  e.stopPropagation();
+                                  break;
+                              }
+                            });
+  lastPage();
+};
+document.addEventListener("DOMContentLoaded", setupDocument);
+
