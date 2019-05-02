8b35d05480b142b3bf5000156618b1bd17782567
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-05-02 13:51:42 +0900

    Update eval/noeval.

--- geometry/eager/toZ0Surface.js
+++ geometry/eager/toZ0Surface.js
@@ -1,6 +1,6 @@
 import { filterAndFlattenAssemblyData } from './filterAndFlattenAssemblyData';
 import { toDisjointGeometry } from './toDisjointGeometry';
-import { union } from '@jsxcad/algorithm-z0surface';
+import { union } from '@jsxcad/geometry-z0surface';
 
 export const toZ0Surface = ({ requires, excludes }, assembly) => {
   const filtered = filterAndFlattenAssemblyData({ requires, excludes, form: 'z0Surface' }, toDisjointGeometry(assembly));
