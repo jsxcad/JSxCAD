7823d418abe3d7bc6061fc2858c6d4321da568bf
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-05-06 23:35:59 +0900

    Update file watcher.

--- sys/writeFile.js
+++ sys/writeFile.js
@@ -12,7 +12,7 @@ export const writeFile = async (options, path, data) => {
     const file = getFile(path);
     file.data = data;
     for (const watcher of file.watchers) {
-      watcher(file, options);
+      watcher(options, file);
     }
   } else {
     throw Error('die');
