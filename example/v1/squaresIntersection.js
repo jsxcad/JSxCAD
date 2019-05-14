cfe97e7922cee0b7926ca6e4c353ec0ef07f6c76
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-05-14 17:36:14 +0900

    Lint.

--- example/v1/squaresIntersection.js
+++ example/v1/squaresIntersection.js
@@ -1,6 +1,6 @@
 import { difference, intersection, square, writePdf } from '@jsxcad/api-v1';
 
 export const main = async () =>
-  await writePdf({ path: 'tmp/squaresIntersection.pdf' },
-                 intersection(difference(square(10), square(9)).translate([-2, -2]),
-                              difference(square(10), square(9)).translate([2, 2])));
+  writePdf({ path: 'tmp/squaresIntersection.pdf' },
+           intersection(difference(square(10), square(9)).translate([-2, -2]),
+                        difference(square(10), square(9)).translate([2, 2])));
