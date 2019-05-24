93371673edf83f76771c0f7ab51edd6f97e71377
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-05-25 00:23:00 +0900

    Missing files.

---
+++ geometry/eager/getPaths.js
@@ -0,0 +1,12 @@
+import { eachItem } from './eachItem';
+
+export const getPaths = (geometry) => {
+  const paths = [];
+  eachItem(geometry,
+           item => {
+             if (item.paths) {
+               paths.push(item.paths);
+             }
+           });
+  return paths;
+};
