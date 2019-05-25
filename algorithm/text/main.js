5b3bd04e9990f36ee15f9085e9cb4c41db2dd95d
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-05-26 02:30:22 +0900

    Revert from fontkit to opentype.

--- algorithm/text/main.js
+++ algorithm/text/main.js
@@ -1,6 +1,7 @@
+/*
 import { scale, translate } from '@jsxcad/geometry-eager';
 
-import fontkit from 'fontkit';
+import fontkit from 'fontkit/index.js';
 import { fromSvgPath } from '@jsxcad/convert-svg';
 import { union } from '@jsxcad/geometry-z0surface';
 
@@ -20,5 +21,33 @@ export const toFont = ({ name }, bytes) => {
     }
     return scale([factor, factor, factor], { z0Surface: union(...pathsets) });
   };
+  return font;
+};
+*/
+
+import { fromSvgPath } from '@jsxcad/convert-svg';
+import { parse } from 'opentype.js';
+import { scale } from '@jsxcad/geometry-eager';
+import { union } from '@jsxcad/geometry-z0surface';
+
+export const toFont = (options = {}, bytes) => {
+  const fontData = parse(bytes.buffer);
+
+  const font = (options, text) => {
+    const { emSize = 1, curveSegments = 32, size = 72, kerning = true, features, hinting = false } = options;
+    const renderingOptions = { kerning, features, hinting };
+    const svgPaths = [];
+    const factor = emSize * 10 / fontData.unitsPerEm;
+    fontData.forEachGlyph(text, 0, 0, size, renderingOptions,
+                          (glyph, x, y, fontSize) => {
+                            svgPaths.push(glyph.getPath(x, y, fontSize, options).toPathData());
+                          });
+    const pathsets = [];
+    for (let { paths } of svgPaths.map(svgPath => fromSvgPath({ curveSegments: curveSegments }, svgPath))) {
+      pathsets.push(paths);
+    }
+    return scale([factor, factor, factor], { z0Surface: union(...pathsets) });
+  };
+
   return font;
 };
