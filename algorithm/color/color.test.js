37463f00243fda378bb87cee87968bc5343edfb9
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-06-07 15:53:57 +0900

    Lint.

--- algorithm/color/color.test.js
+++ algorithm/color/color.test.js
@@ -1,10 +1,10 @@
 import test from 'ava';
 import { toRgb } from './color';
 
-test("Default works.", t => {
+test('Default works.', t => {
   t.is(toRgb(['color/imaginary'], null), null);
 });
 
-test("Blue works.", t => {
+test('Blue works.', t => {
   t.deepEqual(toRgb(['color/blue'], null), [0, 0, 255]);
 });
