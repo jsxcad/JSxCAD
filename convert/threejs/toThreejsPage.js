4b68f87aec15e3eeea37d6a8fc66bd2cce8491bb
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-06-13 16:46:49 +0900

    Update threejs page generation to use cdn.

--- convert/threejs/toThreejsPage.js
+++ convert/threejs/toThreejsPage.js
@@ -10,7 +10,7 @@ export const toThreejsPage = async ({ cameraPosition = [0, 0, 16], title = 'JSxC
   <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
  </head>
  <body>
-  <script type="text/javascript" src="./display.js"></script>
+  <script type="text/javascript" src="https://unpkg.com/@jsxcad/convert-threejs/dist/display.js"></script>
   <script type="text/javascript">JSxCAD = ${JSON.stringify({ threejsGeometry })};</script>
  </body>
 </html>`;
---
+++ example/v1/triangle.js
@@ -0,0 +1,3 @@
+import { triangle } from '@jsxcad/api-v1';
+
+triangle().writeThreejsPage('tmp/triangle.html');
