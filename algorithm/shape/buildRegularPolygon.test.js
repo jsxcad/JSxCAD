d0c15991a6289be76559b1a2a9cceeeec1c10698
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-06-13 11:16:00 +0900

    Fix shape.

--- algorithm/shape/buildRegularPolygon.test.js
+++ algorithm/shape/buildRegularPolygon.test.js
@@ -1,20 +1,14 @@
-import { canonicalize, transform } from '@jsxcad/geometry-polygons';
 import { fromScaling, fromZRotation, multiply } from '@jsxcad/math-mat4';
+import { reallyQuantizeForSpace } from '@jsxcad/math-utils';
 import { unitRegularTrianglePolygon, unitSquarePolygon } from '@jsxcad/data-shape';
 
 import { buildRegularPolygon } from './buildRegularPolygon';
 import { regularPolygonEdgeLengthToRadius } from './regularPolygonEdgeLengthToRadius';
 import test from 'ava';
 
-test('A square', t => {
-  const edgeLength = regularPolygonEdgeLengthToRadius(1, 4);
-  const polygons = canonicalize(transform(multiply(fromZRotation(45 * 0.017453292519943295),
-                                                   fromScaling([edgeLength, edgeLength, 1])),
-                                          [buildRegularPolygon({ edges: 4 })]));
-  t.deepEqual(polygons, [unitSquarePolygon]);
-});
+const canonicalize = (polygon) => polygon.map(point => point.map(value => reallyQuantizeForSpace(value)));
 
 test('A regular triangular polygon', t => {
-  t.deepEqual(canonicalize([buildRegularPolygon({ edges: 3 })]),
+  t.deepEqual([canonicalize(buildRegularPolygon({ edges: 3 }))],
               [unitRegularTrianglePolygon]);
 });
