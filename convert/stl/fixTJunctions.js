907a73a4c06f7f99db9b93ea92b574f6ff467302
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-06-18 22:34:23 +0900

    Reduce cost of fixTJunctions.

--- convert/stl/fixTJunctions.js
+++ convert/stl/fixTJunctions.js
@@ -1,27 +1,22 @@
-import { distance, equals } from '@jsxcad/math-vec3';
+import { add, distance, equals, length, scale, subtract } from '@jsxcad/math-vec3';
+
+import createTree from 'yaot';
+import { eachPoint } from '@jsxcad/geometry-solid';
 
 const copy = ({ solid }) => ({ solid: solid.map(surface => surface.map(path => path.map(point => [...point]))) });
 
 // FIX: This is neither efficient nor principled.
 // We include this fix for now to better understand the cases where it fails.
 
-// FIX: This introduces degenerate polygons.
-
 export const fixTJunctions = (solids) => {
-  const points = new Map();
-
+  const tree = createTree();
+  const points = [];
   for (const { solid } of solids) {
-    for (const surface of solid) {
-      for (const path of surface) {
-        for (const point of path) {
-          points.set(JSON.stringify(point), point);
-        }
-      }
-    }
+    eachPoint({}, point => points.push(...point), solid);
   }
+  tree.init(points);
 
   const fixed = [];
-
   for (const geometry of solids) {
     const { solid } = copy(geometry);
     for (const surface of solid) {
@@ -30,13 +25,17 @@ export const fixTJunctions = (solids) => {
         for (let current = 0; current < path.length; last = current++) {
           const start = path[last];
           const end = path[current];
-          const span = distance(start, end);
+          const midpoint = scale(0.5, add(start, end));
+          const direction = subtract(end, start);
+          const span = length(direction);
+          const matches = tree.intersectSphere(...midpoint, span / 2 + 1);
           const colinear = [];
-          for (const [, point] of points) {
+          for (const match of matches) {
+            const point = points.slice(match, match + 3);
             if (equals(point, start)) continue;
             if (equals(point, end)) continue;
             if (distance(start, point) + distance(point, end) === span) {
-              // The point is approximately colinear.
+              // The point is approximately colinear and upon the segment.
               colinear.push(point);
             }
           }
