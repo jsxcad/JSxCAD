bd705f200e1d09564052303a79c35400d4b01611
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-06-19 13:02:06 +0900

    Refactor plane inference and re-add cache.

---
+++ math/plane/fromPolygon.js
@@ -0,0 +1,26 @@
+import { dot, length, scale } from '@jsxcad/math-vec3';
+
+const X = 0;
+const Y = 1;
+const Z = 2;
+const W = 3;
+
+// Newell's method for computing the plane of a polygon.
+export const fromPolygon = (polygon) => {
+  const normal = [0, 0, 0];
+  const reference = [0, 0, 0];
+  let lastPoint = polygon[polygon.length - 1];
+  for (const thisPoint of polygon) {
+    normal[X] += (lastPoint[Y] - thisPoint[Y]) * (lastPoint[Z] + thisPoint[Z]);
+    normal[Y] += (lastPoint[Z] - thisPoint[Z]) * (lastPoint[X] + thisPoint[X]);
+    normal[Z] += (lastPoint[X] - thisPoint[X]) * (lastPoint[Y] + thisPoint[Y]);
+    reference[X] += lastPoint[X];
+    reference[Y] += lastPoint[Y];
+    reference[Z] += lastPoint[Z];
+    lastPoint = thisPoint;
+  }
+  const factor = 1 / length(normal);
+  const plane = scale(factor, normal);
+  plane[W] = dot(reference, normal) * factor / polygon.length;
+  return plane;
+};
--- math/plane/fromPolygon.test.js
+++ math/plane/fromPolygon.test.js
@@ -1,8 +1,8 @@
+import { fromPolygon } from './fromPolygon';
 import test from 'ava';
-import { toPlane } from './toPlane';
 
 test('Colinear points.', t => {
   // The first three points are colinear.
-  const plane = toPlane([[-4, -4, -1], [0, -4, -1], [4, -4, -1], [4, -5, -1]]);
+  const plane = fromPolygon([[-4, -4, -1], [0, -4, -1], [4, -4, -1], [4, -5, -1]]);
   t.deepEqual(plane, [0, 0, -1, 1]);
 });
