55b042144b92a0964826a1e7378a4012b9d77cb9
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-06-30 01:03:12 +0900

    Micro-optimization.

--- geometry/surface/measureBoundingBox.js
+++ geometry/surface/measureBoundingBox.js
@@ -5,14 +5,18 @@ import { eachPoint } from './eachPoint';
 // returns an array of two Vector3Ds (minimum coordinates and maximum coordinates)
 export const measureBoundingBox = (surface) => {
   if (surface.measureBoundingBox === undefined) {
-    let max = surface[0][0];
-    let min = surface[0][0];
-    eachPoint({},
-              point => {
-                max = maxOfVec3(max, point);
-                min = minOfVec3(min, point);
-              },
-              surface);
+    const min = [Infinity, Infinity, Infinity];
+    const max = [-Infinity, -Infinity, -Infinity];
+    for (const path of surface) {
+      for (const point of path) {
+        if (point[0] < min[0]) min[0] = point[0];
+        if (point[1] < min[1]) min[1] = point[1];
+        if (point[2] < min[2]) min[2] = point[2];
+        if (point[0] > max[0]) max[0] = point[0];
+        if (point[1] > max[1]) max[1] = point[1];
+        if (point[2] > max[2]) max[2] = point[2];
+      }
+    }
     surface.measureBoundingBox = [min, max];
   }
   return surface.measureBoundingBox;
