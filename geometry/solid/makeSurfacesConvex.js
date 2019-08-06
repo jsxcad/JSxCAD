59aecb83c64965e16c3fa69aff6699fad613b659
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-08-06 14:17:56 +0900

    Align vertices before making solid surfaces convex.

--- geometry/solid/makeSurfacesConvex.js
+++ geometry/solid/makeSurfacesConvex.js
@@ -2,9 +2,9 @@ import { alignVertices } from './alignVertices';
 import { assertGood } from './assertGood';
 import { makeConvex } from '@jsxcad/geometry-surface';
 
-export const makeSurfacesConvex = (options = {}, solid) => {
+export const makeSurfacesConvex = (options = {}, rawSolid) => {
+  const solid = alignVertices(rawSolid);
   assertGood(solid);
   const convex = solid.map(surface => makeConvex(options, surface));
-  const aligned = alignVertices(convex);
-  return aligned;
+  return alignVertices(convex);
 };
