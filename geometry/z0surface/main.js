d2599ecd26bbf529104a3ebc84e03e03a1fc2cb8
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-08-16 10:20:34 +0900

    Restore z0surface ops.

--- geometry/z0surface/main.js
+++ geometry/z0surface/main.js
@@ -1,9 +1,11 @@
 import { canonicalize, rotateZ, scale, transform, translate } from './ops';
-import { difference, intersection, union } from '@jsxcad/algorithm-flatten-boolean';
 
 import { clean } from './clean';
+import { difference } from './difference';
+import { intersection } from './intersection';
 import { makeConvex } from './makeConvex';
 import { measureBoundingBox } from './measureBoundingBox';
+import { union } from './union';
 
 export {
   canonicalize,
