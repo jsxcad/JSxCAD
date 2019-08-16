b41aa46414eb72445dd5b0d1304d6696d08924d3
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-08-16 12:38:17 +0900

    Revert ldraw.

--- geometry/z0surface/main.js
+++ geometry/z0surface/main.js
@@ -2,6 +2,7 @@ import { canonicalize, rotateZ, scale, transform, translate } from './ops';
 
 import { clean } from './clean';
 import { difference } from './difference';
+import { doesNotOverlap } from './doesNotOverlap';
 import { intersection } from './intersection';
 import { makeConvex } from './makeConvex';
 import { measureBoundingBox } from './measureBoundingBox';
@@ -11,6 +12,7 @@ export {
   canonicalize,
   clean,
   difference,
+  doesNotOverlap,
   makeConvex,
   measureBoundingBox,
   intersection,
