deb4d00bb69519b52f22774aa19b553fcaf06abf
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-08-19 23:10:48 +0900

    Clean up.

--- geometry/solid/createNormalize4.js
+++ geometry/solid/createNormalize4.js
@@ -6,54 +6,6 @@ const Y = 1;
 const Z = 2;
 const W = 3;
 
-export const createNormalize4New = () => {
-  const map = new Map();
-  const normalize4 = (coordinate) => {
-    // Apply a spatial quantization to the 4 dimensional coordinate.
-    const nx = Math.round(coordinate[X] * multiplier);
-    const ny = Math.round(coordinate[Y] * multiplier);
-    const nz = Math.round(coordinate[Z] * multiplier);
-    const nw = Math.round(coordinate[W] * multiplier);
-    // Look for an existing inhabitant.
-    const value = map.get(`${nx}/${ny}/${nz}/${nw}`);
-    if (value !== undefined) {
-      return value;
-    }
-    // One of the ~0 or ~1 values will match the rounded values above.
-    // The other will match the adjacent cell.
-    const nx0 = Math.floor(coordinate[X] * multiplier);
-    const ny0 = Math.floor(coordinate[Y] * multiplier);
-    const nz0 = Math.floor(coordinate[Z] * multiplier);
-    const nw0 = Math.floor(coordinate[W] * multiplier);
-    const nx1 = nx0 + 1;
-    const ny1 = ny0 + 1;
-    const nz1 = nz0 + 1;
-    const nw1 = nw0 + 1;
-    // Populate the space of the quantized value and its adjacencies.
-    // const normalized = coordinate;
-    const normalized = [nx1 / multiplier, ny1 / multiplier, nz1 / multiplier, nw1 / multiplier];
-    map.set(`${nx0}/${ny0}/${nz0}/${nw0}`, normalized);
-    map.set(`${nx0}/${ny0}/${nz0}/${nw1}`, normalized);
-    map.set(`${nx0}/${ny0}/${nz1}/${nw0}`, normalized);
-    map.set(`${nx0}/${ny0}/${nz1}/${nw1}`, normalized);
-    map.set(`${nx0}/${ny1}/${nz0}/${nw0}`, normalized);
-    map.set(`${nx0}/${ny1}/${nz0}/${nw1}`, normalized);
-    map.set(`${nx0}/${ny1}/${nz1}/${nw0}`, normalized);
-    map.set(`${nx0}/${ny1}/${nz1}/${nw1}`, normalized);
-    map.set(`${nx1}/${ny0}/${nz0}/${nw0}`, normalized);
-    map.set(`${nx1}/${ny0}/${nz0}/${nw1}`, normalized);
-    map.set(`${nx1}/${ny0}/${nz1}/${nw0}`, normalized);
-    map.set(`${nx1}/${ny0}/${nz1}/${nw1}`, normalized);
-    map.set(`${nx1}/${ny1}/${nz0}/${nw0}`, normalized);
-    map.set(`${nx1}/${ny1}/${nz0}/${nw1}`, normalized);
-    map.set(`${nx1}/${ny1}/${nz1}/${nw0}`, normalized);
-    map.set(`${nx1}/${ny1}/${nz1}/${nw1}`, normalized);
-    // This is now the normalized value for this region.
-    return normalized;
-  };
-  return normalize4;
-};
-
 export const createNormalize4 = () => {
   const map = new Map();
   const normalize4 = (coordinate) => {
@@ -79,6 +31,8 @@ export const createNormalize4 = () => {
     const nw1 = nw0 + 1;
     // Populate the space of the quantized value and its adjacencies.
     // const normalized = [nx1 / multiplier, ny1 / multiplier, nz1 / multiplier, nw1 / multiplier];
+    // FIX: Rename the function to reflect that it seems that we cannot quantize planes,
+    // but we can form a consensus among nearby planes.
     const normalized = coordinate;
     map.set(`${nx0}/${ny0}/${nz0}/${nw0}`, normalized);
     map.set(`${nx0}/${ny0}/${nz0}/${nw1}`, normalized);
