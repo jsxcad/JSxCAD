1442e6b1f8e5ffcda273094e5a15bf3f353e63e9
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-08-29 01:14:05 +0900

    Optimize surface difference.

--- geometry/z0surface/measureBoundingBox.js
+++ geometry/z0surface/measureBoundingBox.js
@@ -7,12 +7,14 @@ export const measureBoundingBox = (surface) => {
   if (surface.measureBoundingBox === undefined) {
     let max = surface[0][0];
     let min = surface[0][0];
-    eachPoint({},
-              point => {
-                max = maxOfVec3(max, point);
-                min = minOfVec3(min, point);
-              },
-              surface);
+    for (const polygon of surface) {
+      for (const point of polygon) {
+        if (point[0] < min[0]) min[0] = point[0];
+        if (point[1] < min[1]) min[1] = point[1];
+        if (point[0] > max[0]) max[0] = point[0];
+        if (point[1] > max[1]) max[1] = point[1];
+      }
+    }
     surface.measureBoundingBox = [min, max];
   }
   return surface.measureBoundingBox;
