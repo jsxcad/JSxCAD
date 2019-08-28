c5b56af895bea0917b36ebd5cf060dcf8ba9f563
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-08-28 20:30:18 +0900

    Clean up, center svg origin, and fix svg units.

--- geometry/surface/makeSimple.js
+++ geometry/surface/makeSimple.js
@@ -1,6 +1,6 @@
 import { toPlane, transform } from './ops';
 import { toXYPlaneTransforms } from '@jsxcad/math-plane';
-import { union } from '@jsxcad/geometry-z0surface';
+import { union } from '@jsxcad/geometry-z0surface-boolean';
 
 export const makeSimple = (options = {}, surface) => {
   const [to, from] = toXYPlaneTransforms(toPlane(surface));
