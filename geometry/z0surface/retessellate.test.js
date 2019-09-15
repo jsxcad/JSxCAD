3c41aca3979e6b5a77e355f2803cca1f9f5d7a2c
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-09-15 23:12:47 +0900

    Add tests and fix empty disjoint and kept geometry.

--- geometry/z0surface/retessellate.test.js
+++ geometry/z0surface/retessellate.test.js
@@ -22,3 +22,11 @@ test('top and right', (t) => {
   const r = retessellate([top, left]);
   t.deepEqual(r, [[[-5, 5], [-8, 0], [-5, -5]], [[5, 5], [0, 8], [-5, 5]]]);
 });
+
+test('overlapping', t => {
+  const a = [[0, -5, 0], [10, -5, 0], [10, 5, 0], [0, 5, 0]];
+  const b = [[0, -5, 0], [10, -5, 0], [10, 5, 0], [0, 5, 0]];
+  const r = retessellate([a, b]);
+  // This is producing duplicates
+  t.deepEqual(r, [[[10, -5], [10, 5], [0, 5], [0, -5]], [[10, -5], [10, 5], [0, 5], [0, -5]]]);
+});
