803a1fba3e3d37b5dc50c9e1f53f3272881fc677
BarbourSmith <BarbourSmith@users.noreply.github.com>
2019-09-21 18:29:44 +0300

    Complete paths

--- algorithm/toolpath/overcut.js
+++ algorithm/toolpath/overcut.js
@@ -1,7 +1,22 @@
+import { fromPoints, intersectPointOfLines } from '@jsxcad/math-line2';
 import { add, normalize, rotateZ, scale, subtract } from '@jsxcad/math-vec3';
 import { getSurfaces, getZ0Surfaces } from '@jsxcad/geometry-tagged';
 
 import { getEdges } from '@jsxcad/geometry-path';
+
+
+const intersectionPoints = (cuts) => {
+    cuts.push(cuts[0])
+    var intersectionPoints = [];
+    var i = 0;
+    while(i < cuts.length-1){
+        const point = intersectPointOfLines(fromPoints(...cuts[i]), fromPoints(...cuts[i+1]));
+        point.push(cuts[i][0][2])
+        intersectionPoints.push(point)
+        i++;
+    }
+    return intersectionPoints;
+}
 
 export const overcutPathEdges = (path, radius = 1, overcut = 0) => {
   const cuts = [];
@@ -16,19 +31,20 @@ export const overcutPathEdges = (path, radius = 1, overcut = 0) => {
     const endCut = add(end, add(frontcut, offset));
     cuts.push([startCut, endCut]);
   }
-  return cuts;
+  var points = intersectionPoints(cuts)
+  return points;
 };
 
 export const overcut = (geometry, radius = 1, overcut = 0) => {
   const cuts = [];
   for (const { surface } of getSurfaces(geometry)) {
     for (const path of surface) {
-      cuts.push(...overcutPathEdges(path, radius, overcut));
+      cuts.push(overcutPathEdges(path, radius, overcut));
     }
   }
   for (const { z0Surface } of getZ0Surfaces(geometry)) {
     for (const path of z0Surface) {
-      cuts.push(...overcutPathEdges(path, radius, overcut));
+      cuts.push(overcutPathEdges(path, radius, overcut));
     }
   }
   return cuts;
