5a52e516aaa25e605077d1c5f911a6364f262754
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-11-06 20:00:34 +0900

    Lint

--- convert/threejs/toSvg.test.js
+++ convert/threejs/toSvg.test.js
@@ -8,7 +8,7 @@ import { scale as scaleSurface } from '@jsxcad/geometry-surface';
 import test from 'ava';
 import { toSvg } from './toSvg';
 
-const { writeFile, readFile } = fs.promises;
+const { readFile } = fs.promises;
 
 test('Example', async (t) => {
   const svg = await toSvg(
