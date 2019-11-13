12f97ebced3991b9a32342bfafa122b16cddb796
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-11-13 20:43:41 +0900

    Fix tests.

--- sys/readFile.test.js
+++ sys/readFile.test.js
@@ -4,13 +4,13 @@ import test from 'ava';
 
 test('Utf8 read', async t => {
   setupFilesystem({ fileBase: 'tmp' });
-  const data = await readFile({ as: 'utf8', sources: [{ file: 'testdata/hello.txt' }] }, 'utf8');
+  const data = await readFile({ as: 'utf8', sources: [{ file: 'file/testdata/hello.txt' }] }, 'utf8');
   t.is(data, 'Hello\n');
 });
 
 test('Bytes read', async t => {
   setupFilesystem({ fileBase: 'tmp' });
-  const data = await readFile({ as: 'bytes', sources: [{ file: 'testdata/hello.txt' }] }, 'bytes');
+  const data = await readFile({ as: 'bytes', sources: [{ file: 'file/testdata/hello.txt' }] }, 'bytes');
   t.deepEqual([0, 1, 2, 3, 4, 5].map(nth => data[nth]),
               [72, 101, 108, 108, 111, 10]);
 });
