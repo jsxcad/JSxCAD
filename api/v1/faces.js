eb738e1195e40acba3862e6171cd271b40ca9c21
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-11-20 10:16:52 +0900

    wip

--- api/v1/faces.js
+++ api/v1/faces.js
@@ -1,16 +1,48 @@
 import Shape from './Shape';
+import { alignVertices } from '@jsxcad/geometry-solid';
 import { getSolids } from '@jsxcad/geometry-tagged';
 
 export const faces = (shape, xform = (_ => _)) => {
-  const faces = [];
+  let nextFaceId = 0;
+  const faces = new Set();
+  const vertexFaces = new Map();
+  const rememberFace = (vertex, face) => {
+    if (!faces.has(face)) {
+      faces.add(face);
+      face.faceId = nextFaceId++;
+      face.faceNeighbors = new Set();
+    }
+    if (!vertexFaces.has(vertex)) {
+      vertexFaces.set(vertex, []);
+    }
+    vertexFaces.get(vertex).push(face);
+  };
   for (const { solid } of getSolids(shape.toKeptGeometry())) {
-    for (const surface of solid) {
+    const alignedSolid = alignVertices(solid);
+    for (const surface of alignedSolid) {
       for (const face of surface) {
-        faces.push(xform(face));
+        for (const vertex of face) {
+          rememberFace(vertex, face);
+        }
       }
     }
   }
-  return faces;
+  for (const face of faces) {
+    for (const vertex of face) {
+      for (const neighbor of vertexFaces.get(vertex)) {
+        if (neighbor !== face) {
+          for (const neighborVertex of neighbor) {
+            if (vertex !== neighborVertex) {
+              if (vertexFaces.get(neighborVertex).includes(face)) {
+                face.faceNeighbors.add(neighbor);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return [...faces].map(xform);
 };
 
 const facesMethod = function (...args) { return faces(this, ...args); };
