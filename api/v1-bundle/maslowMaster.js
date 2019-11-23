63f0daeb55cd8b3df54584f2c0f7c1b8ea9d70f4
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-11-23 11:00:22 +0900

    Clean up

--- api/v1-bundle/maslowMaster.js
+++ api/v1-bundle/maslowMaster.js
@@ -2,30 +2,17 @@ import { createService } from '@jsxcad/sys';
 
 window.bootstrap = async () => {
   const agent = async ({ ask, question }) => {
-    if (question.ask) {
-      const { identifier, options } = question.ask;
-      return askSys(identifier, options);
-    } else if (question.readFile) {
-      const { options, path } = question.readFile;
-      return readFile(options, path);
-    } else if (question.writeFile) {
-      const { options, path, data } = question.writeFile;
-      return writeFile(options, path, data);
-    } else if (question.deleteFile) {
-      const { options, path } = question.deleteFile;
-      return deleteFile(options, path);
-    } else if (question.log) {
-      const { entry } = question.log;
-      return log(entry);
-    }
+    // This just handles questions from the worker.
+    console.log(`Was asked: ${JSON.stringify(question)}`);
   };
 
   const { ask } = await createService({ webWorker: './maslowWorker.js', agent });
 
   const result = await ask({
-                              values: [10,10],
-                              key: "rectangle"
-                           });
+    values: [10, 10],
+    key: 'rectangle'
+  });
+
   console.log(JSON.stringify(result));
 };
 
