0d6106480d84c8c294ddd96f003bbcebbf407023
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-11-24 16:05:22 +0900

    Initial auth support.

---
+++ api/v1-bundle/auth.js
@@ -0,0 +1,21 @@
+/* global postMessage, onmessage:writable, self */
+
+import { writeFile } from '@jsxcad/sys';
+
+Error.stackTraceLimit = Infinity;
+
+window.bootstrap = async () => {
+  const { search } = location;
+  // We expect something like: '?github=xxx'
+  if (search.startsWith('?github=')) {
+    const accessToken = search.substring(8);
+    await writeFile({ project: '.system' }, 'auth/github/accessToken', accessToken);
+  }
+  window.close();
+};
+
+document.onreadystatechange = () => {
+  if (document.readyState === 'complete') {
+    window.bootstrap();
+  }
+};
