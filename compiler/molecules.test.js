70fe4a2aead78c96068b870030b61e9d99c5dd86
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-11-28 15:15:15 +0900

    Add initial molecules normalization

--- compiler/molecules.test.js
+++ compiler/molecules.test.js
@@ -1,10 +1,13 @@
-import test from 'ava';
-import { toDot } from './molecules';
 import { readFileSync, writeFileSync } from 'fs';
+import { toDotFromFlows, toFlows } from './molecules';
 
-test("Basic", t => {
+import test from 'ava';
+
+test('toFlows', t => {
   const data = JSON.parse(readFileSync('molecule.json'));
-  const dot = toDot(data);
-  writeFileSync('molecule.dot', dot);
-  t.is(dot, '');
-})
+  const observed = toFlows(data);
+  writeFileSync('molecule-observed.flow', JSON.stringify(observed, null, '  '));
+  writeFileSync('molecule-observed.dot', toDotFromFlows(observed));
+  const expected = JSON.parse(readFileSync('molecule-expected.flow'));
+  t.deepEqual(observed, expected);
+});
