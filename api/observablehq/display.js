a7020133e2279ca831120ecb981da7f8b76d0ded
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-12-13 23:24:55 +0900

    Update hq api

--- api/observablehq/display.js
+++ api/observablehq/display.js
@@ -2,11 +2,11 @@ import { Shape } from '@jsxcad/api-v1';
 import { staticDisplay } from '@jsxcad/ui-threejs';
 import { toThreejsGeometry } from '@jsxcad/convert-threejs';
 
-export const display = (shape, { view, page = {} } = {}) => {
-  const { width = 1024, height = 512 } = page;
+export const display = (shape, { view, width = 1024, height = 512 } = {}) => {
   const threejsGeometry = toThreejsGeometry(shape.toKeptGeometry());
   const { canvas } = staticDisplay({ view, threejsGeometry },
                                    { offsetWidth: width, offsetHeight: height });
+  canvas.style = `width: ${width}px; height: ${height}px`;
   return canvas;
 };
 
--- es6/jsxcad-api-observablehq.js
+++ es6/jsxcad-api-observablehq.js
@@ -1,27 +1,17 @@
-import * as api from './jsxcad-api-v1.js';
 import { Shape } from './jsxcad-api-v1.js';
+import * as jsxcadApiV1_js from './jsxcad-api-v1.js';
+export { jsxcadApiV1_js as api };
 import { staticDisplay } from './jsxcad-ui-threejs.js';
 import { toThreejsGeometry } from './jsxcad-convert-threejs.js';
 
-const display = (shape, { view, page = {} } = {}) => {
-  const { width = 1024, height = 512 } = page;
+const display = (shape, { view, width = 1024, height = 512 } = {}) => {
   const threejsGeometry = toThreejsGeometry(shape.toKeptGeometry());
   const { canvas } = staticDisplay({ view, threejsGeometry },
                                    { offsetWidth: width, offsetHeight: height });
+  canvas.style = `width: ${width}px; height: ${height}px`;
   return canvas;
 };
 
 const displayMethod = function (...args) { return display(this, ...args); };
 
 Shape.prototype.display = displayMethod;
-
-/**
- *
- * Defines the interface used by the api to access the rest of the system on
- * behalf of a user. e.g., algorithms and geometries.
- *
- * A user can destructively update this mapping in their code to change what
- * the api uses.
- */
-
-export default api;
