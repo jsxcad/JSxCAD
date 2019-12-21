a1b64c71471686d3b93cf4c7cf4964bf07e81fad
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-12-21 22:23:04 +0900

    pack

--- es6/jsxcad-api-observablehq.js
+++ es6/jsxcad-api-observablehq.js
@@ -1,13 +1,21 @@
 import { Shape } from './jsxcad-api-v1.js';
 export * from './jsxcad-api-v1.js';
-import './jsxcad-ui-threejs.js';
-import './jsxcad-convert-threejs.js';
+import { staticDisplay } from './jsxcad-ui-threejs.js';
+import { toThreejsGeometry } from './jsxcad-convert-threejs.js';
 
-const bigViewMethod = function ({ width = 512, height = 256, view = { position: [100, -100, 100] } } = {}) { return view(this, { width, height, view }); };
-const bigTopViewMethod = function ({ width = 512, height = 256, view = { position: [0, 0, 100] } } = {}) { return view(this, { width, height, view }); };
-const viewMethod = function ({ width = 256, height = 128, view = { position: [100, -100, 100] } } = {}) { return view(this, { width, height, view }); };
-const topViewMethod = function ({ width = 256, height = 128, view = { position: [0, 0, 100] } } = {}) { return view(this, { width, height, view }); };
-const frontViewMethod = function ({ width = 256, height = 128, view = { position: [0, -100, 0] } } = {}) { return view(this, { width, height, view }); };
+const buildView = (shape, { view, width = 256, height = 128 } = {}) => {
+  const threejsGeometry = toThreejsGeometry(shape.toKeptGeometry());
+  const { canvas } = staticDisplay({ view, threejsGeometry },
+                                   { offsetWidth: width, offsetHeight: height });
+  canvas.style = `width: ${width}px; height: ${height}px`;
+  return canvas;
+};
+
+const bigViewMethod = function ({ width = 512, height = 256, view = { position: [100, -100, 100] } } = {}) { return buildView(this, { width, height, view }); };
+const bigTopViewMethod = function ({ width = 512, height = 256, view = { position: [0, 0, 100] } } = {}) { return buildView(this, { width, height, view }); };
+const viewMethod = function ({ width = 256, height = 128, view = { position: [100, -100, 100] } } = {}) { return buildView(this, { width, height, view }); };
+const topViewMethod = function ({ width = 256, height = 128, view = { position: [0, 0, 100] } } = {}) { return buildView(this, { width, height, view }); };
+const frontViewMethod = function ({ width = 256, height = 128, view = { position: [0, -100, 0] } } = {}) { return buildView(this, { width, height, view }); };
 
 Shape.prototype.view = viewMethod;
 Shape.prototype.bigView = bigViewMethod;
