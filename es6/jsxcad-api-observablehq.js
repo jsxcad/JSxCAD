ce1e1f7f3b4f5842b01a7e7f732e866de85fef38
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-12-24 17:10:44 +0900

    Use canvas instead of webgl context for staticDisplay

--- es6/jsxcad-api-observablehq.js
+++ es6/jsxcad-api-observablehq.js
@@ -3,11 +3,37 @@ export * from './jsxcad-api-v1.js';
 import { staticDisplay } from './jsxcad-ui-threejs.js';
 import { toThreejsGeometry } from './jsxcad-convert-threejs.js';
 
+/* global document */
+
+const toCanvasFromWebglContext = (webgl) => {
+  // Derived from https://github.com/Jam3/webgl-to-canvas2d
+  const { width, height } = webgl.canvas;
+  const inImageData = new Uint8Array(width * height * 4);
+  webgl.readPixels(0, 0, width, height, webgl.RGBA, webgl.UNSIGNED_BYTE, inImageData);
+
+  const outCanvas = document.createElement('canvas');
+  outCanvas.width = width;
+  outCanvas.height = height;
+  const outContext = outCanvas.getContext('2d');
+  const outImageData = outContext.getImageData(0, 0, width, height);
+  outImageData.data.set(new Uint8ClampedArray(inImageData));
+
+  outContext.putImageData(outImageData, 0, 0);
+  outContext.translate(0, height);
+  outContext.scale(1, -1);
+  outContext.drawImage(outCanvas, 0, 0);
+  outContext.setTransform(1, 0, 0, 1, 0, 0);
+
+  return outCanvas;
+};
+
 const view = (shape, { view, width = 256, height = 128 } = {}) => {
   const threejsGeometry = toThreejsGeometry(shape.toKeptGeometry());
-  const { canvas } = staticDisplay({ view, threejsGeometry },
-                                   { offsetWidth: width, offsetHeight: height });
+  const { renderer } = staticDisplay({ view, threejsGeometry },
+                                     { offsetWidth: width, offsetHeight: height });
+  const canvas = toCanvasFromWebglContext(renderer.getContext());
   canvas.style = `width: ${width}px; height: ${height}px`;
+  renderer.forceContextLoss();
   return canvas;
 };
 
