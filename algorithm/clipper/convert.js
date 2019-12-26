589b4575124d840a728616c4d127aacc8463b405
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-12-26 16:07:07 +0900

    wip

--- algorithm/clipper/convert.js
+++ algorithm/clipper/convert.js
@@ -7,11 +7,4 @@ const toInt = (integer) => integer * 1e7;
 const toFloat = (integer) => integer / 1e7;
 
 export const fromSurface = (surface, normalize) => surface.map(path => path.map(point => { const [X, Y] = normalize(point); return new IntPoint(toInt(X), toInt(Y)); }));
-export const toSurface = (paths, normalize) => {
-  const result = paths.map(path => path.map(({ X, Y }) => { return normalize([toFloat(X), toFloat(Y)]); }));
-  for (const path of result) {
-    const area = measureArea(path);
-    // assertGood(path);
-  }
-  return result;
-}
+export const toSurface = (paths, normalize) => paths.map(path => path.map(({ X, Y }) => { return normalize([toFloat(X), toFloat(Y)]); }));
