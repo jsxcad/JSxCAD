2b04dc0f3687ad9bcc3c25b30b46f39972b6bfa9
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-01-03 23:47:41 +0900

    Update

---
+++ es6/jsxcad-api-v1-shapefile.js
@@ -0,0 +1,34 @@
+import { readFile, getSources } from './jsxcad-sys.js';
+import Shape from './jsxcad-api-v1-shape.js';
+import { fromShapefile } from './jsxcad-convert-shapefile.js';
+
+/**
+ *
+ * # Read Shapefile
+ *
+ * ::: illustration { "view": { "position": [0, 0, 100] } }
+ * ```
+ *
+ * await readShapefile({ shpPath: 'ne_50m_admin_0_countries.shp', dbfPath: 'ne_50m_admin_0_countries.dbf' });
+ * ```
+ * :::
+ *
+ **/
+
+const readShapefile = async (options) => {
+  const { shpPath, dbfPath } = options;
+  let shpData = await readFile({ as: 'bytes', ...options }, `source/${shpPath}`);
+  if (shpData === undefined) {
+    shpData = await readFile({ as: 'bytes', sources: getSources(`cache/${shpPath}`), ...options }, `cache/${shpPath}`);
+  }
+  let dbfData = await readFile({ as: 'bytes', ...options }, `source/${dbfPath}`);
+  if (dbfData === undefined) {
+    dbfData = await readFile({ as: 'bytes', sources: getSources(`cache/${dbfPath}`), ...options }, `cache/${dbfPath}`);
+  }
+  return Shape.fromGeometry(await fromShapefile(options, shpData, dbfData));
+};
+
+const api = { readShapefile };
+
+export default api;
+export { readShapefile };
