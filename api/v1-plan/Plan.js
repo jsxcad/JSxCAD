7f2083688f9755727af596b5b885582e181f363e
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-01-03 23:22:40 +0900

    Missing files

---
+++ api/v1-plan/Plan.js
@@ -0,0 +1,9 @@
+import Shape from '@jsxcad/api-v1-shape';
+
+export const Plan = ({ plan, marks = [], planes = [], tags = [], visualization }, context) => {
+  let geometry = visualization === undefined ? { assembly: [] } : visualization.toKeptGeometry();
+  const shape = Shape.fromGeometry({ plan, marks, planes, tags, visualization: geometry }, context);
+  return shape;
+};
+
+export default Plan;
