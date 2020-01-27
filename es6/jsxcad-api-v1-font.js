247199702299616cc81e40cb0376f85c35383247
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-01-27 17:50:47 +0900

    Update

--- es6/jsxcad-api-v1-font.js
+++ es6/jsxcad-api-v1-font.js
@@ -68,10 +68,10 @@ Hershey.toPaths = toPaths;
 
 const toEmSizeFromMm = (mm) => mm * 1.5;
 
-const readFont = async (path, { url } = {}) => {
+const readFont = async (path, { src } = {}) => {
   let data = await readFile({ as: 'bytes' }, `source/${path}`);
   if (data === undefined) {
-    data = await readFile({ as: 'bytes', sources: [url] }, `cache/${path}`);
+    data = await readFile({ as: 'bytes', sources: [src] }, `cache/${path}`);
   }
   const font = toFont({ path }, data);
   const fontFactory = (size = 1) => (text) => Shape.fromGeometry(font({ emSize: toEmSizeFromMm(size) }, text));
