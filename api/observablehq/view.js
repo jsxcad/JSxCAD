eae13535d4f12e47b8f76f87e9a4b3669d296180
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-02-09 12:51:59 +0900

    Add dataUrl and image methods

--- api/observablehq/view.js
+++ api/observablehq/view.js
@@ -37,6 +37,15 @@ export const staticView = async (shape, { target, position, up, width = 256, hei
   return canvas;
 };
 
+export const dataURL = async (...args) => (await staticView(...args)).toDataURL('png');
+
+export const image = async (...args) => {
+  const image = document.createElement('img');
+  const dataUrl = (await staticView(...args)).toDataURL('png');
+  image.src = dataUrl;
+  return image;
+};
+
 export const orbitView = async (shape, { target, position, up, width = 256, height = 128 } = {}) => {
   const container = document.createElement('div');
   container.style = `width: ${width}px; height: ${height}px`;
@@ -54,14 +63,38 @@ const viewMethod = async function ({ width = 256, height = 128, position = [100,
 const topViewMethod = async function ({ width = 256, height = 128, position = [0, 0, 100] } = {}) { return staticView(this, { width, height, position }); };
 const frontViewMethod = async function ({ width = 256, height = 128, position = [0, -100, 0] } = {}) { return staticView(this, { width, height, position }); };
 
-const orbitViewMethod = async function ({ width = 512, height = 512, position = [100, -100, 100] } = {}) { return orbitView(this, { width, height, position }); };
-const bigOrbitViewMethod = async function ({ width = 1024, height = 1024, position = [100, -100, 100] } = {}) { return orbitView(this, { width, height, position }); };
-
 Shape.prototype.view = viewMethod;
 Shape.prototype.bigView = bigViewMethod;
 Shape.prototype.topView = topViewMethod;
 Shape.prototype.bigTopView = bigTopViewMethod;
 Shape.prototype.frontView = frontViewMethod;
+
+const bigDataUrlMethod = async function ({ width = 512, height = 256, position = [100, -100, 100] } = {}) { return staticView(this, { width, height, position }); };
+const bigTopDataUrlMethod = async function ({ width = 512, height = 256, position = [0, 0, 100] } = {}) { return staticView(this, { width, height, position }); };
+const dataUrlMethod = async function ({ width = 256, height = 128, position = [100, -100, 100] } = {}) { return staticView(this, { width, height, position }); };
+const topDataUrlMethod = async function ({ width = 256, height = 128, position = [0, 0, 100] } = {}) { return staticView(this, { width, height, position }); };
+const frontDataUrlMethod = async function ({ width = 256, height = 128, position = [0, -100, 0] } = {}) { return staticView(this, { width, height, position }); };
+
+Shape.prototype.dataUrl = dataUrlMethod;
+Shape.prototype.bigDataUrl = bigDataUrlMethod;
+Shape.prototype.topDataUrl = topDataUrlMethod;
+Shape.prototype.bigTopDataUrl = bigTopDataUrlMethod;
+Shape.prototype.frontDataUrl = frontDataUrlMethod;
+
+const bigImageMethod = async function ({ width = 512, height = 256, position = [100, -100, 100] } = {}) { return staticView(this, { width, height, position }); };
+const bigTopImageMethod = async function ({ width = 512, height = 256, position = [0, 0, 100] } = {}) { return staticView(this, { width, height, position }); };
+const imageMethod = async function ({ width = 256, height = 128, position = [100, -100, 100] } = {}) { return staticView(this, { width, height, position }); };
+const topImageMethod = async function ({ width = 256, height = 128, position = [0, 0, 100] } = {}) { return staticView(this, { width, height, position }); };
+const frontImageMethod = async function ({ width = 256, height = 128, position = [0, -100, 0] } = {}) { return staticView(this, { width, height, position }); };
+
+Shape.prototype.image = imageMethod;
+Shape.prototype.bigImage = bigImageMethod;
+Shape.prototype.topImage = topImageMethod;
+Shape.prototype.bigTopImage = bigTopImageMethod;
+Shape.prototype.frontImage = frontImageMethod;
+
+const orbitViewMethod = async function ({ width = 512, height = 512, position = [100, -100, 100] } = {}) { return orbitView(this, { width, height, position }); };
+const bigOrbitViewMethod = async function ({ width = 1024, height = 1024, position = [100, -100, 100] } = {}) { return orbitView(this, { width, height, position }); };
 
 Shape.prototype.orbitView = orbitViewMethod;
 Shape.prototype.bigOrbitView = bigOrbitViewMethod;
