4ea5b68331f501b4917f4a25d9fb9f9551d9f823
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-02-09 11:33:00 +0900

    Observable

--- api/observablehq/view.js
+++ api/observablehq/view.js
@@ -38,25 +38,24 @@ export const staticView = async (shape, { target, position, up, width = 256, hei
 };
 
 export const orbitView = async (shape, { target, position, up, width = 256, height = 128 } = {}) => {
+  throw Error('die: ' + orbitDisplay);
+/*
   const threejsGeometry = toThreejsGeometry(shape.toKeptGeometry());
   const container = document.createElement('div');
   container.style = `width: ${width}px; height: ${height}px`;
   await orbitDisplay({ view: { target, position, up }, threejsGeometry }, container);
   return container;
+*/
 };
 
-// Work around the name collision in destructuring.
-const buildOrbitView = async (...args) => orbitView(...args);
-const buildStaticView = async (...args) => staticView(...args);
+const bigViewMethod = async function ({ width = 512, height = 256, position = [100, -100, 100] } = {}) { return staticView(this, { width, height, position }); };
+const bigTopViewMethod = async function ({ width = 512, height = 256, position = [0, 0, 100] } = {}) { return staticView(this, { width, height, position }); };
+const viewMethod = async function ({ width = 256, height = 128, position = [100, -100, 100] } = {}) { return staticView(this, { width, height, position }); };
+const topViewMethod = async function ({ width = 256, height = 128, position = [0, 0, 100] } = {}) { return staticView(this, { width, height, position }); };
+const frontViewMethod = async function ({ width = 256, height = 128, position = [0, -100, 0] } = {}) { return staticView(this, { width, height, position }); };
 
-const bigViewMethod = async function ({ width = 512, height = 256, position = [100, -100, 100] } = {}) { return buildStaticView(this, { width, height, position }); };
-const bigTopViewMethod = async function ({ width = 512, height = 256, position = [0, 0, 100] } = {}) { return buildStaticView(this, { width, height, position }); };
-const viewMethod = async function ({ width = 256, height = 128, position = [100, -100, 100] } = {}) { return buildStaticView(this, { width, height, position }); };
-const topViewMethod = async function ({ width = 256, height = 128, position = [0, 0, 100] } = {}) { return buildStaticView(this, { width, height, position }); };
-const frontViewMethod = async function ({ width = 256, height = 128, position = [0, -100, 0] } = {}) { return buildStaticView(this, { width, height, position }); };
-
-const orbitViewMethod = async function ({ width = 512, height = 512, position = [100, -100, 100] } = {}) { return buildOrbitView(this, { width, height, position }); };
-const bigOrbitViewMethod = async function ({ width = 1024, height = 1024, position = [100, -100, 100] } = {}) { return buildOrbitView(this, { width, height, position }); };
+const orbitViewMethod = async function ({ width = 512, height = 512, position = [100, -100, 100] } = {}) { return orbitView(this, { width, height, position }); };
+const bigOrbitViewMethod = async function ({ width = 1024, height = 1024, position = [100, -100, 100] } = {}) { return orbitView(this, { width, height, position }); };
 
 Shape.prototype.view = viewMethod;
 Shape.prototype.bigView = bigViewMethod;
