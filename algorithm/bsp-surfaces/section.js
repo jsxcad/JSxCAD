e067aff0ea95d5d500b18d905e6ff66f0523046c
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-02-16 22:36:14 +0900

    Clean up bsp

--- algorithm/bsp-surfaces/section.js
+++ algorithm/bsp-surfaces/section.js
@@ -1,8 +1,8 @@
-import { removeExteriorPolygons, fromSolid as toBspFromSolid } from './bsp';
+import { removeExteriorPolygonsForSection, fromSolid as toBspFromSolid } from './bsp';
 
 import { alignVertices } from '@jsxcad/geometry-solid';
 
 export const section = (solid, surfaces, normalize) => {
   const bsp = toBspFromSolid(alignVertices(solid, normalize), normalize);
-  return surfaces.map(surface => removeExteriorPolygons(bsp, surface, normalize));
+  return surfaces.map(surface => removeExteriorPolygonsForSection(bsp, surface, normalize));
 };
