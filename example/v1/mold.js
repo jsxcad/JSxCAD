e7141cdf45085975c30f18c6b3b377fb13a1dbdc
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-02-16 20:24:53 +0900

    Update examples

--- example/v1/mold.js
+++ example/v1/mold.js
@@ -4,5 +4,5 @@ const outside = Triangle.ofDiameter(64);
 const inside = Triangle.ofDiameter(60);
 const perimeter = outside.cut(inside);
 
-await inside.extrude(1).writeStl('stl/mold_inside.stl');
-await perimeter.extrude(6).writeStl('stl/mold_perimeter.stl');
+await inside.extrude(1).Item().Page().writeStl('mold_inside');
+await perimeter.extrude(6).Item().Page().writeStl('mold_perimeter');
