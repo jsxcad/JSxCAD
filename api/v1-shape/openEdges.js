0601a4ad537a17507a25d81d43c1166841d43500
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-02-26 00:04:49 +0900

    Add watertightness to solid fromPolygons.

--- api/v1-shape/openEdges.js
+++ api/v1-shape/openEdges.js
@@ -1,12 +1,16 @@
+import { getAnySurfaces, getSolids } from '@jsxcad/geometry-tagged';
+
 import Shape from './Shape';
 import { findOpenEdges } from '@jsxcad/geometry-solid';
-import { getSolids } from '@jsxcad/geometry-tagged';
 
 export const openEdges = (shape, { isOpen = true } = {}) => {
   const r = (v) => v;
   const paths = [];
   for (const { solid } of getSolids(shape.toKeptGeometry())) {
     paths.push(...findOpenEdges(solid, isOpen));
+  }
+  for (const { surface, z0Surface } of getAnySurfaces(shape.toKeptGeometry())) {
+    paths.push(...findOpenEdges([surface || z0Surface], isOpen));
   }
   return Shape.fromGeometry({ paths: paths.map(path => path.map(([x, y, z]) => [r(x), r(y), r(z)])) });
 };
--- example/v1/chainHull.js
+++ example/v1/chainHull.js
@@ -1,9 +1,9 @@
 import '@jsxcad/api-v1-stl';
 
-await ChainedHull(
+await Empty().add(ChainedHull(
         Circle(10).moveZ(-10),
         Square(5),
-        Circle(10).moveZ(10))
+        Circle(10).moveZ(10)))
   .Item()
   .Page()
   .writeStl('chainHull');
