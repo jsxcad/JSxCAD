4560df5c926fe30abb088bda69278ccd5972ccd1
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-03-02 00:21:15 +0900

    Intersection via rewrite

--- geometry/tagged/difference.js
+++ geometry/tagged/difference.js
@@ -9,62 +9,6 @@ import { difference as solidDifference } from '@jsxcad/geometry-solid-boolean';
 import { difference as surfaceDifference } from '@jsxcad/geometry-surface-boolean';
 import { difference as z0SurfaceDifference } from '@jsxcad/geometry-z0surface-boolean';
 
-/*
-const differenceImplOld = (baseGeometry, ...geometries) => {
-  if (baseGeometry.item) {
-    return { ...baseGeometry, item: difference(baseGeometry.item, ...geometries) };
-  }
-
-  const result = { disjointAssembly: [] };
-  // Solids.
-  const solids = geometries.flatMap(geometry => getSolids(geometry)).filter(isNegative).map(item => item.solid);
-  for (const { solid, tags } of getSolids(baseGeometry)) {
-    result.disjointAssembly.push({ solid: solidDifference(solid, ...solids), tags });
-  }
-  // Surfaces -- generalize to surface unless specializable upon z0surface.
-  const z0Surfaces = geometries.flatMap(geometry => getZ0Surfaces(geometry).filter(isNegative).map(item => item.z0Surface));
-  const surfaces = geometries.flatMap(geometry => getSurfaces(geometry).filter(isNegative).map(item => item.surface));
-  for (const { z0Surface, tags } of getZ0Surfaces(baseGeometry)) {
-    if (surfaces.length === 0) {
-      result.disjointAssembly.push({ z0Surface: z0SurfaceDifference(z0Surface, ...z0Surfaces), tags });
-    } else {
-      result.disjointAssembly.push({ surface: surfaceDifference(z0Surface, ...z0Surfaces, ...surfaces), tags });
-    }
-  }
-  for (const { surface, tags } of getSurfaces(baseGeometry)) {
-    result.disjointAssembly.push({ surface: surfaceDifference(surface, ...surfaces, ...z0Surfaces), tags });
-  }
-  // Paths.
-  const pathsets = geometries.flatMap(geometry => getPaths(geometry)).filter(isNegative).map(item => item.paths);
-  for (const { paths, tags } of getPaths(baseGeometry)) {
-    result.disjointAssembly.push({ paths: pathsDifference(paths, ...pathsets), tags });
-  }
-  // Plans are preserved over difference.
-  for (const plan of getPlans(baseGeometry)) {
-    result.disjointAssembly.push(plan);
-  }
-  for (const geometry of geometries) {
-    for (const plan of getPlans(geometry)) {
-      result.disjointAssembly.push(plan);
-    }
-  }
-  // Connections
-  for (const connection of getConnections(baseGeometry)) {
-    result.disjointAssembly.push(connection);
-  }
-  // Items
-  for (const item of getItems(baseGeometry)) {
-    result.disjointAssembly.push(item);
-  }
-  // Points
-  for (const points of getPoints(baseGeometry)) {
-    // FIX: Actually subtract points.
-    result.disjointAssembly.push(points);
-  }
-  return result;
-};
-*/
-
 const differenceImpl = (geometry, ...geometries) => {
   const op = (geometry, descend) => {
     if (geometry.solid) {
