17fb6ad046ce11e6f7971d7c9c67cdd9dcbd4981
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-04-01 07:32:41 +0900

    Fixes for maslowWorker

--- es6/jsxcad-api-v1-shapes.js
+++ es6/jsxcad-api-v1-shapes.js
@@ -1,56 +1,8 @@
-import Shape, { difference, intersection } from './jsxcad-api-v1-shape.js';
-import { emit } from './jsxcad-sys.js';
-import { ensurePages } from './jsxcad-api-v1-plans.js';
-import { getLeafs, getAnySurfaces, getPaths, rewriteTags } from './jsxcad-geometry-tagged.js';
 import { concatenate, rotateZ, translate } from './jsxcad-geometry-path.js';
+import Shape, { difference, intersection } from './jsxcad-api-v1-shape.js';
 import { numbers, linear } from './jsxcad-api-v1-math.js';
 import { buildRegularPolygon, toRadiusFromApothem as toRadiusFromApothem$1, regularPolygonEdgeLengthToRadius, buildPolygonFromPoints, buildRegularPrism, buildFromFunction, buildFromSlices, buildRegularIcosahedron, buildRingSphere, buildRegularTetrahedron } from './jsxcad-algorithm-shape.js';
-
-// FIX: We shouldn't need to supply a path to this.
-const view = (shape, { width = 1024, height = 512, position = [100, -100, 100] } = {}) => {
-  for (const entry of ensurePages(shape.toKeptGeometry())) {
-    for (let leaf of getLeafs(entry.content)) {
-      emit({ geometry: { width, height, position, geometry: leaf } });
-    }
-  }
-  return shape;
-};
-
-Shape.prototype.view = function ({ width = 512, height = 256, position = [100, -100, 100] } = {}) {
-  return view(this, { width, height, position });
-};
-
-Shape.prototype.smallView = function ({ width = 256, height = 128, position = [100, -100, 100] } = {}) {
-  return view(this, { width, height, position });
-};
-
-Shape.prototype.bigView = function ({ width = 1024, height = 512, position = [100, -100, 100] } = {}) {
-  return view(this, { width, height, position });
-};
-
-Shape.prototype.topView = function ({ width = 512, height = 256, position = [0, 0, 100] } = {}) {
-  return view(this, { width, height, position });
-};
-
-Shape.prototype.smallTopView = function ({ width = 256, height = 128, position = [0, 0, 100] } = {}) {
-  return view(this, { width, height, position });
-};
-
-Shape.prototype.bigTopView = function ({ width = 1024, height = 512, position = [0, 0, 100] } = {}) {
-  return view(this, { width, height, position });
-};
-
-Shape.prototype.frontView = function ({ width = 512, height = 256, position = [0, -100, 0] } = {}) {
-  return view(this, { width, height, position });
-};
-
-Shape.prototype.smallFrontView = function ({ width = 256, height = 128, position = [0, -100, 0] } = {}) {
-  return view(this, { width, height, position });
-};
-
-Shape.prototype.bigFrontView = function ({ width = 1024, height = 512, position = [0, -100, 0] } = {}) {
-  return view(this, { width, height, position });
-};
+import { getAnySurfaces, getPaths, rewriteTags } from './jsxcad-geometry-tagged.js';
 
 /**
  *
