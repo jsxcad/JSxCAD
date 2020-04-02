82e2ee34b8f6a623084edc12425490914d611294
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-04-02 09:26:43 +0900

    wip

--- compiler/toEcmascript.js
+++ compiler/toEcmascript.js
@@ -36,7 +36,10 @@ const fromObjectExpression = ({ properties }) => {
   return object;
 };
 
-export const toEcmascript = (options, script) => {
+const nullImport = () => 'return {};';
+
+export const toEcmascript = (script, options = {}) => {
+  const { importer = nullImport } = options;
   const parseOptions = {
     allowAwaitOutsideFunction: true,
     allowReturnOutsideFunction: true,
@@ -45,14 +48,9 @@ export const toEcmascript = (options, script) => {
   let ast = parse(script, parseOptions);
 
   const exportNames = [];
-  const expressions = [];
-
   const body = ast.body;
   const out = [];
-  const annotations = { imports: {} };
 
-  // Separate top level exports and expressions.
-  // FIX: This will reorder things unnecessarily when export main is present.
   for (let nth = 0; nth < body.length; nth++) {
     const entry = body[nth];
     if (entry.type === 'ExportNamedDeclaration') {
@@ -78,53 +76,31 @@ export const toEcmascript = (options, script) => {
       //
       // FIX: Handle other variations.
       const { specifiers, source } = entry;
+      const module = importer(source.value, options);
 
-      if (specifiers.length === 0) {
-        out.push(parse(`await importModule('${source.value}');`, parseOptions));
-      } else {
+      out.push(parse(`const $module = (() => { ${module} })();`, parseOptions));
+
+      if (specifiers.length > 0) {
         for (const { imported, local, type } of specifiers) {
           switch (type) {
-            case 'ImportDefaultSpecifier':
-              out.push(parse(`const ${local.name} = (await importModule('${source.value}')).default;`, parseOptions));
+            case 'ImportDefaultSpecifier': {
+              out.push(parse(`const ${local.name} = $module.default;`, parseOptions));
               break;
-            case 'ImportSpecifier':
-              out.push(parse(`const { ${imported.name} } = await importModule('${source.value}');`, parseOptions));
+            }
+            case 'ImportSpecifier': {
+              out.push(parse(`const { ${imported.name} } = $module;`, parseOptions));
               break;
+            }
           }
         }
       }
-    } else if (entry.type === 'ExpressionStatement' && entry.expression.type === 'ObjectExpression') {
-      Object.assign(annotations, fromObjectExpression(entry.expression));
-    } else if (entry.type === 'ExpressionStatement' && entry.expression.type === 'CallExpression' && entry.expression.callee.name === 'source') {
-      // source('a', 'b') needs to be kept at the top level to support imports and avoid repetition.
-      out.push(entry);
     } else {
-      expressions.push(entry);
+      out.push(entry);
     }
   }
 
-  // Set up a main.
-  if (exportNames.length > 0) {
-    // They export something, so assume they know what they're doing.
-    out.push(...expressions);
-  } else {
-    // They don't export a main function, so build one for them.
-    if (expressions.length >= 1) {
-      // Turn any final expression into a return statement.
-      const last = expressions.length - 1;
-      const tail = expressions[last];
-      if (tail.type === 'ExpressionStatement') {
-        expressions[last] = parse(`return ${generate(expressions[last])}`, parseOptions);
-      }
-    }
-    const main = parse(`const main = async () => { ${expressions.map(expression => generate(expression)).join('\n')} };`, parseOptions);
-    out.push(main);
-    exportNames.push('main');
-  }
-
-  // Return the exports as an object.
   out.push(parse(`return { ${exportNames.join(', ')} };`, parseOptions));
 
-  const result = generate(parse(`return async () => { ${out.map(generate).join('\n')} };`, parseOptions));
+  const result = '\n' + generate(parse(out.map(generate).join('\n'), parseOptions));
   return result;
 };
--- compiler/toEcmascript.test.js
+++ compiler/toEcmascript.test.js
@@ -4,107 +4,70 @@ import { toEcmascript } from './toEcmascript';
 Error.stackTraceLimit = Infinity;
 
 test('Wrap and return.', t => {
-  const ecmascript = toEcmascript({},
-                                  `export const foo = (x) => x + 1;
+  const ecmascript = toEcmascript(`export const foo = (x) => x + 1;
                                    export const main = async () => {
                                      let a = 10;
                                      return circle(foo(a));
                                    }`);
   t.is(ecmascript,
-       `return async () => {
-  const foo = x => x + 1;
-  const main = async () => {
-    let a = 10;
-    return circle(foo(a));
-  };
-  return {
-    foo,
-    main
-  };
+`
+const foo = x => x + 1;
+const main = async () => {
+  let a = 10;
+  return circle(foo(a));
+};
+return {
+  foo,
+  main
 };
 `);
 });
 
 test("Don't return declarations.", t => {
-  const ecmascript = toEcmascript({},
-                                  `let a = 10;`);
+  const ecmascript = toEcmascript(`let a = 10;`);
   t.is(ecmascript,
-       `return async () => {
-  const main = async () => {
-    let a = 10;
-  };
-  return {
-    main
-  };
-};
+`
+let a = 10;
+return {};
 `);
 });
 
 test('Bind await to calls properly.', t => {
-  const ecmascript = toEcmascript({}, `foo().bar()`);
+  const ecmascript = toEcmascript(`foo().bar()`);
   t.is(ecmascript,
-       `return async () => {
-  const main = async () => {
-    return foo().bar();
-  };
-  return {
-    main
-  };
-};
+`
+foo().bar();
+return {};
 `);
 });
 
 test('Top level await.', t => {
-  const ecmascript = toEcmascript({}, `await foo()`);
+  const ecmascript = toEcmascript(`await foo()`);
   t.is(ecmascript,
-       `return async () => {
-  const main = async () => {
-    return await foo();
-  };
-  return {
-    main
-  };
-};
+`
+await foo();
+return {};
 `);
 });
 
 test('Wrap on long implicit return expression is not malformed.', t => {
-  const ecmascript = toEcmascript({}, `
+  const ecmascript = toEcmascript(`
 foo();
 // Hello.
 await bar({ aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaagh: 1 }, 2);
 `);
   t.is(ecmascript,
-       `return async () => {
-  const main = async () => {
-    foo();
-    return await bar({
-      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaagh: 1
-    }, 2);
-  };
-  return {
-    main
-  };
-};
-`);
-});
-
-test('Source stays at top level to support import', t => {
-  const ecmascript = toEcmascript({}, `source('a', 'b'); foo();`);
-  t.is(ecmascript, `return async () => {
-  source('a', 'b');
-  const main = async () => {
-    return foo();
-  };
-  return {
-    main
-  };
-};
+`
+foo();
+await bar({
+  aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaagh: 1
+}, 2);
+return {};
 `);
 });
 
 test('Import', t => {
-  const ecmascript = toEcmascript({}, 'import { foo } from "bar";');
+  const ecmascript = toEcmascript('import { foo } from "bar";');
   t.is(ecmascript, `return async () => {
   const {foo} = await importModule('bar');
   const main = async () => {};
@@ -115,8 +78,9 @@ test('Import', t => {
 `);
 });
 
+/*
 test('Default Import', t => {
-  const ecmascript = toEcmascript({}, 'import Foo from "bar";');
+  const ecmascript = toEcmascript('import Foo from "bar";');
   t.is(ecmascript, `return async () => {
   const Foo = (await importModule('bar')).default;
   const main = async () => {};
@@ -128,7 +92,7 @@ test('Default Import', t => {
 });
 
 test('Unassigned Import', t => {
-  const ecmascript = toEcmascript({}, 'import "bar";');
+  const ecmascript = toEcmascript('import "bar";');
   t.is(ecmascript, `return async () => {
   await importModule('bar');
   const main = async () => {};
@@ -138,3 +102,4 @@ test('Unassigned Import', t => {
 };
 `);
 });
+*/
