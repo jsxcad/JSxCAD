3aee1f1962b6977861e6f3a0b09560473390acd6
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-04-07 23:42:14 +0900

    Add Noise

---
+++ api/v1-math/Noise.js
@@ -0,0 +1,9 @@
+import { makeNoise3D } from 'open-simplex-noise';
+
+export const Noise = (seed = 0) => {
+  const generator = makeNoise3D(seed);
+  const generate = (x = 0, y = 0, z = 0) => generator(x, y, z);
+  return generate;
+};
+
+export default Noise;
