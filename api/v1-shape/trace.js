c44ba28f794930a0e0ee949cfea6b3aa873499de
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-06-16 21:25:56 +0900

    wip

---
+++ api/v1-shape/trace.js
@@ -0,0 +1,22 @@
+import Shape from './Shape';
+import { getPaths } from '@jsxcad/geometry-tagged';
+import { segment } from '@jsxcad/geometry-paths';
+
+export const trace = (shape, length = 1) => {
+  const tracePaths = [];
+  for (const { paths } of getPaths(shape.toKeptGeometry())) {
+    for (let start = 0;; start += length) {
+      const segments = segment(paths, start, start + length);
+      if (segments.length === 0) {
+        break;
+      }
+      tracePaths.push(...segments);
+    }
+  }
+  return Shape.fromGeometry({ paths: tracePaths, tags: ['display/trace'] });
+};
+
+const traceMethod = function (length = 1) {
+  return trace(this, length);
+};
+Shape.prototype.trace = traceMethod;
