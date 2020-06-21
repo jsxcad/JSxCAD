ba8f1bab1b7005b6187a684655ec5733918e6704
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-06-21 10:01:13 +0900

    wip

--- math/plane/toPolygon.js
+++ math/plane/toPolygon.js
@@ -6,8 +6,10 @@ export const toPolygon = (plane, size = 1e10) => {
   const v = unit(cross(plane, random(plane)));
   const u = cross(v, plane);
   const origin = scale(plane[W], plane);
-  return [add(origin, scale(-size, u)),
-          add(origin, scale(-size, v)),
-          add(origin, scale(size, u)),
-          add(origin, scale(size, v))];
+  return [
+    add(origin, scale(-size, u)),
+    add(origin, scale(-size, v)),
+    add(origin, scale(size, u)),
+    add(origin, scale(size, v)),
+  ];
 };
