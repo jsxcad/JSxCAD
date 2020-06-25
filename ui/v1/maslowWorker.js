05a636365a55a2e62cdf8f8280097dfbc199e6fd
BarbourSmith <BarbourSmith@users.noreply.github.com>
2020-06-25 14:31:32 -0700

    Lint

--- ui/v1/maslowWorker.js
+++ ui/v1/maslowWorker.js
@@ -3,14 +3,126 @@
 import * as api from '@jsxcad/api-v1';
 import * as convertThree from '@jsxcad/convert-threejs';
 import * as sys from '@jsxcad/sys';
+import { add, normalize, scale, subtract } from '@jsxcad/math-vec3';
 import { intersection, union } from '@jsxcad/api-v1-shape';
 import { Hull } from '@jsxcad/api-v1-extrude';
 import { clearCache } from '@jsxcad/cache';
 // import { pack } from '@jsxcad/api-v1-layout';
 import { toStl } from '@jsxcad/convert-stl';
 import { toSvg } from '@jsxcad/convert-svg';
-import { add, length, normalize, scale, subtract } from '@jsxcad/math-vec3';
-import { getEdges } from '@jsxcad/geometry-path';
+
+function computeXYSegmentLength(p1, p2) {
+    const segmentLength = Math.sqrt((p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]));
+    return segmentLength;
+}
+
+function computeXYPathLength(path) {
+    var pathLength = 0;
+    var k = 1;
+    while (k < path.length) {
+        const p1 = path[k - 1];
+        const p2 = path[k];
+        pathLength = pathLength + computeXYSegmentLength(p1, p2);
+        k++;
+    }
+
+    return pathLength;
+}
+
+// Move all the points in a range to a new z-value
+function shiftRange(path, start, end, height) {
+    var generatedPath = [path[0]];
+    var n = 1;
+    while (n < path.length) {
+        // Measure the distance with the new point
+        const nextTotalLength = computeXYPathLength(generatedPath.concat([path[n]]));
+        if (nextTotalLength > start && nextTotalLength < end) {
+            var shiftedPoint = path[n];
+            shiftedPoint[2] = height;
+            generatedPath.push(shiftedPoint);
+        } else {
+            generatedPath.push(path[n]);
+        }
+        n++;
+    }
+    return path;
+}
+
+// Insert an elevation change a given distance into a path
+function insertPointByDist(path, target, height) {
+    var generatedPath = [path[0]];
+    // Walk through each point and see if adding it would excede distance
+    var k = 1;
+    while (k < path.length) {
+        // Measure the distance with the new point
+        const nextTotalLength = computeXYPathLength(generatedPath.concat([path[k]]));
+        if (nextTotalLength > target) {
+            const prior = computeXYPathLength(generatedPath);
+            const newSegmentLength = target - prior;
+
+            const normalVector = normalize(subtract(path[k], path[k - 1]));
+            const scaled = scale(newSegmentLength, normalVector);
+
+            var newPoint = add(path[k - 1], scaled);
+            newPoint[2] = height; // Set the new point's z value
+
+            generatedPath.push(newPoint);
+
+            // Fastforward to the end
+            while (k < path.length) {
+                generatedPath.push(path[k]);
+                k++;
+            }
+
+            return generatedPath;
+        }
+        generatedPath.push(path[k]);
+        k++;
+    }
+    // This return catches it if no point is added
+    return generatedPath;
+}
+
+// Add tabs to a pass
+function addTabs(pass, baselineHeight, thickness, tabs, toolSize) {
+    const tabHeight = thickness * (-2 / 3);
+    console.log(tabHeight);
+    // Compute path length
+    const totalPathLength = computeXYPathLength(pass.concat([pass[0]]));
+    const tabWidth = 4 * toolSize;
+    const spaceBetweenTabs = (totalPathLength - (tabWidth * tabs)) / tabs;
+    const bias = (totalPathLength / 8) - (tabWidth / 2); // Bias the tabs so on a square they appear on the middle of the edges, not at the corners
+
+    var m = 1;
+    while (m <= tabs) {
+        const tabBegin = m * spaceBetweenTabs + (m - 1) * tabWidth - bias;
+        const tabEnd = m * spaceBetweenTabs + (m - 1) * tabWidth + tabWidth - bias;
+        pass = insertPointByDist(pass, tabBegin, baselineHeight);
+        pass = insertPointByDist(pass, tabBegin, tabHeight);
+        pass = shiftRange(pass, tabBegin, tabEnd, tabHeight);
+        pass = insertPointByDist(pass, tabEnd, tabHeight);
+        pass = insertPointByDist(pass, tabEnd, baselineHeight);
+        m++;
+    }
+
+    return pass;
+}
+
+// Essentially moves a pass to the correct depth
+function generatePass(path, depth, hasTabs, thickness, tabs, toolSize) {
+    var pass = [];
+    // Add all the rest of the points in the pass
+    path.forEach(point => {
+      pass.push([point[0], point[1], depth]);
+    });
+    pass.push([path[0][0], path[0][1], depth]); // Add the first point again to close the path
+
+    if (hasTabs) {
+        pass = addTabs(pass, depth, thickness, tabs, toolSize);
+    }
+
+    return pass;
+}
 
 const say = (message) => postMessage(message);
 const agent = async ({ ask, question }) => {
@@ -83,8 +195,8 @@ const agent = async ({ ask, question }) => {
         return overcutShape.cut(sweep).toKeptGeometry();
       case 'render':
         var fromGeo = api.Empty(); // This should be an empty geometry;
-        
-        try{
+
+        try {
             if (values[1] === true && values[2] === false) { // Solid, no wireframe
               fromGeo = api.Shape.fromGeometry(values[0]);
             } else if (values[1] === false && values[2] === true) {
@@ -93,8 +205,7 @@ const agent = async ({ ask, question }) => {
               const intermediate = api.Shape.fromGeometry(values[0]);
               fromGeo = intermediate.with(intermediate.outline());
             }
-        }
-        catch(err){
+        } catch (err) {
             console.log("Can't display in worker thread");
             console.log(err);
         }
@@ -120,148 +231,29 @@ const agent = async ({ ask, question }) => {
         const distPerPass = -1 * thickness / numberOfPasses;
 
         var slice = gcodeShape.Union().center().section().toolpath(toolSize, { joinPaths: true });
-        
-        function computeXYSegmentLength(p1, p2){
-            const segmentLength = Math.sqrt((p1[0]-p2[0])*(p1[0]-p2[0])+(p1[1]-p2[1])*(p1[1]-p2[1]));
-            return segmentLength;
-        }
-        
-        function computeXYPathLength(path){
-            var pathLength = 0;
-            var k = 1;
-            while(k < path.length){
-                const p1 = path[k-1];
-                const p2 = path[k];
-                pathLength = pathLength + computeXYSegmentLength(p1,p2);
-                k++;
-            }
-            
-            return pathLength;
-        }
-        
-        //Move all the points in a range to a new z-value
-        function shiftRange(path, start, end, height){
-            var generatedPath = [path[0]];
-            var n = 1;
-            while(n < path.length){
-                // Measure the distance with the new point
-                const nextTotalLength = computeXYPathLength(generatedPath.concat([path[n]]));
-                if(nextTotalLength > start && nextTotalLength < end){
-                    var shiftedPoint = path[n];
-                    shiftedPoint[2]  = height;
-                    generatedPath.push(shiftedPoint);
-                }
-                else{
-                    generatedPath.push(path[n]);
-                }
-                n++;
-            }
-            return path;
-        }
-        
-        //Insert an elevation change a given distance into a path
-        function insertPointByDist(path, target, height){
-            var generatedPath = [path[0]];
-            //Walk through each point and see if adding it would excede distance
-            var k = 1;
-            while(k < path.length){
-                //Measure the distance with the new point
-                const nextTotalLength = computeXYPathLength(generatedPath.concat([path[k]]));
-                if(nextTotalLength > target){
-                    
-                    const prior = computeXYPathLength(generatedPath);
-                    const lastSegment = computeXYSegmentLength(path[k-1], path[k]);
-                    const newSegmentLength = target - prior;
-                    
-                    const normalVector = normalize(subtract(path[k], path[k-1]));
-                    const scaled = scale(newSegmentLength, normalVector);
-                    
-                    var newPoint = add(path[k-1], scaled);
-                    newPoint[2] = height; //Set the new point's z value
-                    
-                    generatedPath.push(newPoint); 
-                    
-                    //Fastforward to the end
-                    while(k < path.length){
-                        generatedPath.push(path[k]);
-                        k++;
-                    }
-                    
-                    return generatedPath;
-                }
-                generatedPath.push(path[k]);
-                k++;
-            }
-            //This return catches it if no point is added
-            return generatedPath;
-        }
-        
-        //Add tabs to a pass
-        function addTabs(pass, baselineHeight){
-            const tabHeight = thickness*(-2/3);
-            console.log(tabHeight)
-            //Compute path length
-            const totalPathLength = computeXYPathLength(pass.concat([pass[0]]));
-            const tabWidth = 4*toolSize;
-            const spaceBetweenTabs = (totalPathLength - (tabWidth*tabs))/tabs;
-            const bias = (totalPathLength/8)-(tabWidth/2); //Bias the tabs so on a square they appear on the middle of the edges, not at the corners
-            
-            var m = 1;
-            while(m <= tabs){
-                const tabBegin = m*spaceBetweenTabs+(m-1)*tabWidth-bias;
-                const tabEnd   = m*spaceBetweenTabs+(m-1)*tabWidth+tabWidth-bias;
-                pass = insertPointByDist(pass, tabBegin, baselineHeight);
-                pass = insertPointByDist(pass, tabBegin, tabHeight);
-                pass = shiftRange(pass, tabBegin, tabEnd, tabHeight);
-                pass = insertPointByDist(pass, tabEnd, tabHeight);
-                pass = insertPointByDist(pass, tabEnd, baselineHeight);
-                m++;
-            }
-            
-            return pass;
-        }
-        
-        //Essentially moves a pass to the correct depth
-        function generatePass(path, depth, hasTabs){
-            var pass = [];
-            //Add all the rest of the points in the pass
-            path.forEach(point => {
-              pass.push([point[0], point[1], depth]);
-            });
-            pass.push([path[0][0], path[0][1], depth]); // Add the first point again to close the path
-            
-            if(hasTabs){
-                pass = addTabs(pass, depth);
-            }
-            
-            return pass;
-        }
-        
+
         // Split each path into it's layers
         slice.geometry.paths.forEach((path, index) => {
-          
-          //This generates all the passes for a single cut (ie a single outside or inside profile)
+          // This generates all the passes for a single cut (ie a single outside or inside profile)
           var plungePoint = path[0];
           plungePoint[2] = safeHeight;
           var newPath = [[0, 0, safeHeight], plungePoint];
           var i = 1;
           while (i <= numberOfPasses) {
-            
-            //Paths in the bottom 1/3 have tabs
+            // Paths in the bottom 1/3 have tabs
             var tabsInThisPass = false;
-            if(i >= numberOfPasses*(2/3)){
+            if (i >= numberOfPasses * (2 / 3)) {
                 tabsInThisPass = true;
             }
-            
-            newPath = newPath.concat(generatePass(path, i*distPerPass, tabsInThisPass));
-            
+
+            newPath = newPath.concat(generatePass(path, i * distPerPass, tabsInThisPass, thickness, tabs, toolSize));
+
             i++;
           }
-          
-          
+
           const lastIndex = newPath.length - 1;
           newPath.push([newPath[lastIndex][0], newPath[lastIndex][1], safeHeight]); // Retract back to safe height after finishing move
-          slice.geometry.paths[index] = newPath; //Replaces the existing path with the new multi level one
+          slice.geometry.paths[index] = newPath; // Replaces the existing path with the new multi level one
         });
 
         // A shape can have many paths which need to be cut (ie inside and outside profiles. This joins them all into one
@@ -269,9 +261,7 @@ const agent = async ({ ask, question }) => {
         slice.geometry.paths.forEach(path => {
           completePath = completePath.concat(path);
         });
-        
-        
-        
+
         slice.geometry.paths = [completePath];
 
         return slice.toKeptGeometry();
