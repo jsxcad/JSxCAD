7a0491c42e91c4b8cbdf210118dd48266bdeebd0
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-07-15 00:37:58 +0900

    Migrate fill to general clip/intersection

--- api/v1-extrude/squash.js
+++ api/v1-extrude/squash.js
@@ -5,10 +5,13 @@ import {
   getSurfaces,
   getZ0Surfaces,
   taggedLayers,
+  taggedPaths,
+  taggedZ0Surface,
+  union,
 } from '@jsxcad/geometry-tagged';
 
 import Shape from '@jsxcad/api-v1-shape';
-import { outline } from '@jsxcad/geometry-z0surface-boolean';
+
 import { toPlane } from '@jsxcad/math-poly3';
 
 export const squash = (shape) => {
@@ -23,11 +26,9 @@ export const squash = (shape) => {
         polygons.push(isCounterClockwise(flat) ? flat : flip(flat));
       }
     }
-    result.content.push({
-      type: 'z0Surface',
-      z0Surface: outline(polygons),
-      tags,
-    });
+    result.content.push(
+      union(...polygons.map((polygon) => taggedZ0Surface({ tags }, [polygon])))
+    );
   }
   for (const { surface, tags } of getSurfaces(geometry)) {
     const polygons = [];
@@ -36,14 +37,14 @@ export const squash = (shape) => {
       if (toPlane(flat) === undefined) continue;
       polygons.push(isCounterClockwise(flat) ? flat : flip(flat));
     }
-    result.content.push({ type: 'z0Surface', z0Surface: polygons, tags });
+    result.content.push(taggedZ0Surface({ tags }, polygons));
   }
   for (const { z0Surface, tags } of getZ0Surfaces(geometry)) {
     const polygons = [];
     for (const path of z0Surface) {
       polygons.push(path);
     }
-    result.content.push({ type: 'z0Surface', z0Surface: polygons, tags });
+    result.content.push(taggedZ0Surface({ tags }, polygons));
   }
   for (const { paths, tags } of getPaths(geometry)) {
     const flatPaths = [];
@@ -51,6 +52,7 @@ export const squash = (shape) => {
       flatPaths.push(path.map(([x, y]) => [x, y, 0]));
     }
     result.content.push({ type: 'paths', paths: flatPaths, tags });
+    result.content.push(taggedPaths({ tags }, flatPaths));
   }
   return Shape.fromGeometry(result);
 };
---
+++ geometry/bsp/fromSurface.js
@@ -0,0 +1,34 @@
+import { add, scale } from '@jsxcad/math-vec3';
+import {
+  outline as outlineSurface,
+  toPlane as toPlaneFromSurface,
+} from '@jsxcad/geometry-surface';
+
+import { fromPolygons } from './bsp.js';
+import { getEdges } from '@jsxcad/geometry-path';
+
+const large = 1e10;
+
+export const fromSurface = (surface, normalize) => {
+  const polygons = [];
+  const normal = toPlaneFromSurface(surface);
+  if (normal === undefined) {
+    // The surface is degenerate.
+    return fromPolygons([]);
+  }
+  const top = scale(large, normal);
+  const bottom = scale(-large, normal);
+  for (const path of outlineSurface(surface, normalize)) {
+    for (const [start, end] of getEdges(path)) {
+      // Build a large wall.
+      polygons.push([
+        add(start, top),
+        add(start, bottom),
+        add(end, bottom),
+        add(end, top),
+      ]);
+    }
+  }
+  // This is an excessively large uncapped prism.
+  return fromPolygons(polygons, normalize);
+};
