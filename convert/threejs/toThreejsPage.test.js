a74f9c9cc15e01ce9cf9e1c2f2fcadefc116b16b
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-08-02 10:18:50 +0900

    Update toThreejsPage

--- convert/threejs/toThreejsPage.test.js
+++ convert/threejs/toThreejsPage.test.js
@@ -12,60 +12,35 @@ import { scale as scaleSurface } from '@jsxcad/geometry-surface';
 import test from 'ava';
 import { toThreejsPage } from './toThreejsPage.js';
 
-const { readFile, writeFile } = fs.promises;
+const { readFile } = fs.promises;
 
 test.beforeEach(async (t) => {
   await boot();
 });
 
-test('No-eval geodesic sphere', async (t) => {
-  const html = await toThreejsPage(
-    {
-      type: 'assembly',
-      content: [
-        {
-          type: 'paths',
-          paths: scalePaths([3, 3, 3], [unitRegularTrianglePolygon]),
-          tags: ['paths'],
-        },
-        {
-          type: 'solid',
-          solid: fromPolygons(unitGeodesicSphere20Polygons),
-          tags: ['solid'],
-        },
-        {
-          type: 'z0Surface',
-          z0Surface: scaleSurface([2, 2, 2], [unitSquarePolygon]),
-          tags: ['surface'],
-        },
-      ],
-    },
-    { includeEvaluator: false }
-  );
-  await writeFile('toThreejsPage.test.noeval.html', html, { encoding: 'utf8' });
+test('Geodesic sphere', async (t) => {
+  const html = await toThreejsPage({
+    type: 'assembly',
+    content: [
+      {
+        type: 'paths',
+        paths: scalePaths([3, 3, 3], [unitRegularTrianglePolygon]),
+        tags: ['paths'],
+      },
+      {
+        type: 'solid',
+        solid: fromPolygons(unitGeodesicSphere20Polygons),
+        tags: ['solid'],
+      },
+      {
+        type: 'z0Surface',
+        z0Surface: scaleSurface([2, 2, 2], [unitSquarePolygon]),
+        tags: ['surface'],
+      },
+    ],
+  });
   t.is(
     new TextDecoder('utf8').decode(html),
-    await readFile('toThreejsPage.test.noeval.html', { encoding: 'utf8' })
-  );
-});
-
-test('Eval lego skeleton sphere', async (t) => {
-  const html = await toThreejsPage(
-    { type: 'assembly', content: [] },
-    {
-      includeEvaluator: true,
-      includeEditor: true,
-      initialScript: `
-const main = async () => {
-   const skeleton = await readLDraw({ part: '6260c01.dat' });
-   await writeStl({ path: 'window/skeleton' }, difference(skeleton.scale([0.1, 0.1, 0.1]), Cube(5)));
- }
-`,
-    }
-  );
-  await writeFile('toThreejsPage.test.eval.html', html, { encoding: 'utf8' });
-  t.is(
-    new TextDecoder('utf8').decode(html),
-    await readFile('toThreejsPage.test.eval.html', { encoding: 'utf8' })
+    await readFile('toThreejsPage.test.html', { encoding: 'utf8' })
   );
 });
