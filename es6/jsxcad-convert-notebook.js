ae3945372af98b59bb967deca5660a6f79647f7c
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-08-02 19:52:57 +0900

    Add notebook conversions.

---
+++ es6/jsxcad-convert-notebook.js
@@ -0,0 +1,37 @@
+const toHtml = async (
+  notebook,
+  { view, title = 'JSxCAD Viewer' } = {}
+) => {
+  const html = `
+<html>
+ <head>
+  <title>${title}</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+ </head>
+ <body>
+  <script type='module'>
+    import { toDomElement } from 'https://gitcdn.link/cdn/jsxcad/JSxCAD/master/es6/jsxcad-ui-notebook.js';
+
+    const notebook = ${JSON.stringify(notebook)};
+
+    const run = async () => {
+      const { width = 1024, height = 1024, position = [0, 0, 100] } = {};
+      const body = document.getElementsByTagName('body')[0];
+      const notebookElement = toDomElement(notebook);
+      body.appendChild(notebookElement);
+    };
+
+    document.onreadystatechange = () => {
+      if (document.readyState === 'complete') {
+        run();
+      }
+    };
+  </script>
+ </body>
+</html>
+`;
+  return new TextEncoder('utf8').encode(html);
+};
+
+export { toHtml };
---
+++ ui/notebook/toDomElement.js
@@ -0,0 +1,31 @@
+// global document
+
+import { Shape } from '@jsxcad/api-v1-shape';
+import { dataUrl } from '@jsxcad/ui-threejs';
+import marked from 'marked';
+
+export const toDomElement = async (notebook) => {
+  const container = document.createElement('div');
+  for (const note of notebook) {
+    if (note.geometry) {
+      const { geometry, width, height, position } = note.geometry;
+      const url = await dataUrl(Shape.fromGeometry(geometry), {
+        width,
+        height,
+        position,
+      });
+      const image = document.createElement('img');
+      image.src = url;
+      container.appendChild(image);
+    }
+    if (note.md) {
+      // Use ''' and '' instead of ``` and `` to avoid escaping.
+      // FIX: Do this in a more principled fashion.
+      const data = note.md.replace(/'''/g, '```').replace(/''/g, '``');
+      const markup = document.createElement('div');
+      markup.innerHTML = marked(data);
+      container.appendChild(markup);
+    }
+  }
+  return container;
+};
