cabf381dac06c88e172e439c218f81eac597b2d0
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-09-24 21:01:05 +0900

    wip

---
+++ api/v1-shell/offset.js
@@ -0,0 +1,33 @@
+import {
+  getNonVoidPaths,
+  taggedGroup,
+  taggedPaths,
+} from '@jsxcad/geometry-tagged';
+
+import { Shape } from '@jsxcad/api-v1-shape';
+import { createNormalize3 } from '@jsxcad/algorithm-quantize';
+import { deduplicate } from '@jsxcad/geometry-path';
+import { offset as offsetAlgorithm } from '@jsxcad/algorithm-occt';
+
+export const offset = (shape, amount = 1) => {
+  const normalize = createNormalize3();
+  const offsetPathsets = [];
+  for (const { tags, paths } of getNonVoidPaths(shape.toDisjointGeometry())) {
+    const offsetPaths = [];
+    // Offset each path separately.
+    for (const path of paths) {
+      offsetPaths.push(
+        ...offsetAlgorithm([deduplicate(path.map(normalize))], amount)
+      );
+    }
+    offsetPathsets.push(taggedPaths({ tags }, offsetPaths));
+  }
+  return Shape.fromGeometry(taggedGroup({}, ...offsetPathsets));
+};
+
+const offsetMethod = function (amount) {
+  return offset(this, amount);
+};
+Shape.prototype.offset = offsetMethod;
+
+export default offset;
