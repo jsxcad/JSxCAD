860ca0c97e13cda5b414463fa2f451601668b788
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-12-23 15:52:38 +0900

    Missing file

---
+++ geometry/tagged/projectToPlane.js
@@ -0,0 +1,49 @@
+import {
+  fromPaths as fromPathsToGraph,
+  projectToPlane as projectToPlaneOfGraph,
+} from '@jsxcad/geometry-graph';
+
+import { rewrite } from './visit.js';
+import { taggedGraph } from './taggedGraph.js';
+import { toTransformedGeometry } from './toTransformedGeometry.js';
+
+export const projectToPlane = (geometry, plane, direction) => {
+  const op = (geometry, descend) => {
+    const { tags } = geometry;
+    switch (geometry.type) {
+      case 'graph': {
+        return taggedGraph(
+          { tags },
+          projectToPlaneOfGraph(geometry.graph, plane, direction)
+        );
+      }
+      case 'solid':
+      case 'z0Surface':
+      case 'surface':
+      case 'points':
+        // Not implemented yet.
+        return geometry;
+      case 'paths':
+        return projectToPlane(
+          taggedGraph({ tags }, fromPathsToGraph(geometry.paths)),
+          plane,
+          direction
+        );
+      case 'plan':
+      case 'assembly':
+      case 'item':
+      case 'disjointAssembly':
+      case 'layers': {
+        return descend();
+      }
+      case 'sketch': {
+        // Sketches aren't real for projectToPlane.
+        return geometry;
+      }
+      default:
+        throw Error(`Unexpected geometry: ${JSON.stringify(geometry)}`);
+    }
+  };
+
+  return rewrite(toTransformedGeometry(geometry), op);
+};
