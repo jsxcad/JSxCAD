3607af142fc3b175630c219ca3282780c63be10a
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-12-31 20:58:39 +0900

    Re-enable implicit functions, epick unstable

--- algorithm/cgal/cgal.cc
+++ algorithm/cgal/cgal.cc
@@ -169,11 +169,46 @@ void FromSurfaceMeshToPolygonSoup(Surface_mesh* mesh, bool triangulate, emscript
   }
 }
 
+#if 1
+Surface_mesh* FromFunctionToSurfaceMesh(double radius, double angular_bound, double radius_bound, double distance_bound, double error_bound, emscripten::val function) {
+  typedef CGAL::Surface_mesh_default_triangulation_3 Tr;
+  // c2t3
+  typedef CGAL::Complex_2_in_triangulation_3<Tr> C2t3;
+  typedef Tr::Geom_traits GT;
+  typedef GT::Sphere_3 Sphere_3;
+  typedef GT::Point_3 Point_3;
+  typedef GT::FT FT;
+  typedef FT (*Function)(Point_3);
+  typedef CGAL::Implicit_surface_3<GT, Function> Surface_3;
+  typedef CGAL::Surface_mesh<Point_3> Epick_Surface_mesh;
+
+  Tr tr;            // 3D-Delaunay triangulation
+  C2t3 c2t3 (tr);   // 2D-complex in 3D-Delaunay triangulation
+  // defining the surface
+  auto op = [&](const Point_3& p) { return FT(function(CGAL::to_double(p.x()), CGAL::to_double(p.y()), CGAL::to_double(p.z())).as<double>()); };
+  Surface_3 surface(op,             // pointer to function
+                    Sphere_3(CGAL::ORIGIN, radius * radius)); // bounding sphere
+  // Note that "2." above is the *squared* radius of the bounding sphere!
+  // defining meshing criteria
+  CGAL::Surface_mesh_default_criteria_3<Tr> criteria(angular_bound,  // angular bound
+                                                     radius_bound,  // radius bound
+                                                     distance_bound); // distance bound
+  // meshing surface
+  CGAL::make_surface_mesh(c2t3, surface, criteria, CGAL::Non_manifold_tag());
+  Epick_Surface_mesh epick_mesh;
+  CGAL::facets_in_complex_2_to_triangle_mesh(c2t3, epick_mesh);
+
+  Surface_mesh* epeck_mesh = new Surface_mesh();
+  copy_face_graph(epick_mesh, *epeck_mesh);
+  return epeck_mesh;
+}
+#endif
+
 #if 0
 Surface_mesh* FromFunctionToSurfaceMesh(double radius, double angular_bound, double radius_bound, double distance_bound, double error_bound, emscripten::val function) {
   // Domain
-  // typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-  typedef CGAL::Exact_predicates_exact_constructions_kernel K;
+  typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+  // typedef CGAL::Exact_predicates_exact_constructions_kernel K;
   typedef K::FT FT;
   typedef K::Point_3 Point;
   typedef FT (Function)(const Point&);
@@ -1981,7 +2016,7 @@ EMSCRIPTEN_BINDINGS(module) {
   emscripten::function("TransformSurfaceMesh", &TransformSurfaceMesh, emscripten::allow_raw_pointers());
   emscripten::function("TransformSurfaceMeshByTransform", &TransformSurfaceMeshByTransform, emscripten::allow_raw_pointers());
   emscripten::function("FromSurfaceMeshToPolygonSoup", &FromSurfaceMeshToPolygonSoup, emscripten::allow_raw_pointers());
-  // emscripten::function("FromFunctionToSurfaceMesh", &FromFunctionToSurfaceMesh, emscripten::allow_raw_pointers());
+  emscripten::function("FromFunctionToSurfaceMesh", &FromFunctionToSurfaceMesh, emscripten::allow_raw_pointers());
   emscripten::function("ComputeConvexHullAsSurfaceMesh", &ComputeConvexHullAsSurfaceMesh, emscripten::allow_raw_pointers());
   emscripten::function("ComputeAlphaShapeAsSurfaceMesh", &ComputeAlphaShapeAsSurfaceMesh, emscripten::allow_raw_pointers());
   emscripten::function("ComputeAlphaShape2AsPolygonSegments", &ComputeAlphaShape2AsPolygonSegments, emscripten::allow_raw_pointers());
--- es6/jsxcad-algorithm-cgal.js
+++ es6/jsxcad-algorithm-cgal.js
@@ -830,8 +830,8 @@ var Module = (function () {
     }
     var wasmMemory;
     var wasmTable = new WebAssembly.Table({
-      initial: 2275,
-      maximum: 2275,
+      initial: 2458,
+      maximum: 2458,
       element: 'anyfunc',
     });
     var ABORT = false;
@@ -1099,9 +1099,9 @@ var Module = (function () {
       Module['HEAPF32'] = HEAPF32 = new Float32Array(buf);
       Module['HEAPF64'] = HEAPF64 = new Float64Array(buf);
     }
-    var STACK_BASE = 5564416,
-      STACK_MAX = 321536,
-      DYNAMIC_BASE = 5564416;
+    var STACK_BASE = 5594544,
+      STACK_MAX = 351664,
+      DYNAMIC_BASE = 5594544;
     assert(STACK_BASE % 16 === 0, 'stack must start aligned');
     assert(DYNAMIC_BASE % 16 === 0, 'heap must start aligned');
     var TOTAL_STACK = 5242880;
