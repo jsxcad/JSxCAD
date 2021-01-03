321daeff10c19b38c0aa0f19a5e9ee601ec3f428
Brian Spilsbury <brian.spilsbury@gmail.com>
2021-01-03 12:50:32 +0900

    Stabilize plane of path construction for the general case

--- geometry/graph/fromPaths.js
+++ geometry/graph/fromPaths.js
@@ -8,10 +8,13 @@ import { arrangePaths, fitPlaneToPoints } from '@jsxcad/algorithm-cgal';
 import { deduplicate, flip, isClockwise } from '@jsxcad/geometry-path';
 
 import { canonicalize as canonicalizePaths } from '@jsxcad/geometry-paths';
+import { dot } from '@jsxcad/math-vec3';
 
 const orientClockwise = (path) => (isClockwise(path) ? path : flip(path));
 const orientCounterClockwise = (path) =>
   isClockwise(path) ? flip(path) : path;
+
+const Z = 2;
 
 // This imposes a planar arrangement.
 export const fromPaths = (inputPaths) => {
@@ -27,6 +30,11 @@ export const fromPaths = (inputPaths) => {
   }
   let plane = fitPlaneToPoints(points);
   if (plane) {
+    // Orient planes up by default.
+    // FIX: Remove this hack.
+    if (dot(plane, [0, 0, 1, 0]) < -0.1) {
+      plane[Z] *= -1;
+    }
     const arrangement = arrangePaths(...plane, paths);
     for (const { points, holes } of arrangement) {
       const face = addFace(graph, { plane });
