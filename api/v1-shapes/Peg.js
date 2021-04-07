7880d9a1c0f795c32ce5e7e3a448cd6fbf4b5c1e
Brian Spilsbury <brian.spilsbury@gmail.com>
2021-04-07 10:39:09 +0900

    Clean up

--- api/v1-shapes/Peg.js
+++ api/v1-shapes/Peg.js
@@ -8,6 +8,7 @@ const Y = 1;
 const Z = 2;
 
 export const Peg = (
+  name,
   origin = [0, 0, 0],
   forward = [0, 1, 0],
   right = [1, 0, 0]
@@ -15,8 +16,12 @@ export const Peg = (
   const o = origin;
   const f = add(origin, forward);
   const r = add(origin, right);
+  const tags = ['peg'];
+  if (name) {
+    tags.push(`peg/${name}`);
+  }
   return Shape.fromGeometry(
-    taggedPoints({ tags: ['peg'] }, [
+    taggedPoints({ tags }, [
       [o[X], o[Y], o[Z], f[X], f[Y], f[Z], r[X], r[Y], r[Z]],
     ])
   );
