6719c3aa57719e680e238c7a3d74afb05520847b
Brian Spilsbury <brian.spilsbury@gmail.com>
2021-06-25 00:07:26 +0900

    Collapse definitions

--- ui/v1/AceEditorLineWidgets.js
+++ ui/v1/AceEditorLineWidgets.js
@@ -1,6 +1,7 @@
 import Ace from 'ace-builds/src-noconflict/ace';
 
+const JavascriptMode = Ace.acequire('ace/mode/javascript').Mode;
 const LineWidgets = Ace.acequire('ace/line_widgets').LineWidgets;
 const Range = Ace.acequire('ace/range').Range;
 
-export const aceEditorLineWidgets = { LineWidgets, Range };
+export const aceEditorLineWidgets = { JavascriptMode, LineWidgets, Range };
