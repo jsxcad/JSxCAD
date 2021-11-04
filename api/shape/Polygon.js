207330e173a92e4f3703d86c31f236d9fc55779e
Brian Spilsbury <brian.spilsbury@gmail.com>
2021-11-04 17:29:37 +0900

    Clean up UI and add printing for gcode.

--- api/shape/Polygon.js
+++ api/shape/Polygon.js
@@ -1,14 +1,8 @@
 import Shape from './Shape.js';
+import { toCoordinate } from './toCoordinate.js';
 
-import { fromPathsToGraph } from '@jsxcad/geometry';
-
-export const Polygon = (...points) => {
-  const path = [];
-  for (const point of points) {
-    point.eachPoint((p) => path.push(p));
-  }
-  return Shape.fromGeometry(fromPathsToGraph({}, [{ points: path }]));
-};
+export const Polygon = (...points) =>
+  Shape.fromClosedPath(points.map((point) => toCoordinate(point)));
 
 export default Polygon;
 
