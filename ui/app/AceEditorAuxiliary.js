e54886be2f6519b4659629347d78d4b4f4dc9f8a
Brian Spilsbury <brian.spilsbury@gmail.com>
2021-11-29 16:41:53 +0900

    Re-enable search and link clicking in editor

--- ui/app/AceEditorAuxiliary.js
+++ ui/app/AceEditorAuxiliary.js
@@ -1,4 +1,5 @@
 import 'ace-builds/src-noconflict/ext-linking';
+import 'ace-builds/src-noconflict/ext-searchbox';
 import 'ace-builds/src-noconflict/mode-javascript';
 import 'ace-builds/src-noconflict/theme-github';
 
