7cd19c4261679402d6a4cfb82ce7ea2c47ae8b3c
Brian Spilsbury <brian.spilsbury@gmail.com>
2022-01-24 18:04:47 +0900

    Missing file

---
+++ api/shape/Segments.js
@@ -0,0 +1,13 @@
+import Shape from './Shape.js';
+
+export const Segments = (...segments) =>
+  Shape.fromSegments(
+    ...Shape.toNestedValues(segments).map(([source, target]) => [
+      Shape.toCoordinate(undefined, source),
+      Shape.toCoordinate(undefined, target),
+    ])
+  );
+
+export default Segments;
+
+Shape.prototype.Segments = Shape.shapeMethod(Segments);
