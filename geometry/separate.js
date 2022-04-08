6d87b0f2975f282c479000bbf2a0301211b0af62
Brian Spilsbury <brian.spilsbury@gmail.com>
2022-04-09 01:40:22 +0900

    Missing files

---
+++ geometry/separate.js
@@ -0,0 +1,22 @@
+import {
+  deletePendingSurfaceMeshes,
+  separate as separateWithCgal,
+} from '@jsxcad/algorithm-cgal';
+
+import { isNotTypeVoid } from './tagged/type.js';
+import { linearize } from './tagged/linearize.js';
+import { taggedGroup } from './tagged/taggedGroup.js';
+import { toConcreteGeometry } from './tagged/toConcreteGeometry.js';
+
+const filter = (geometry) =>
+  ['graph', 'polygonsWithHoles'].includes(geometry.type) &&
+  isNotTypeVoid(geometry);
+
+export const separate = (geometry) => {
+  const concreteGeometry = toConcreteGeometry(geometry);
+  const inputs = [];
+  linearize(concreteGeometry, filter, inputs);
+  const outputs = separateWithCgal(inputs);
+  deletePendingSurfaceMeshes();
+  return taggedGroup({}, ...outputs);
+};
