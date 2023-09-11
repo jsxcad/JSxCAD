d14dba0404c3339bf0b14707ad854f3191a0850d
Brian Spilsbury <brian.spilsbury@gmail.com>
2023-09-12 00:06:45 +0900

    Arrange notebooks as sorted accordions

--- ui/app/Notebook.js
+++ ui/app/Notebook.js
@@ -6,10 +6,12 @@ import * as PropTypes from 'prop-types';
 
 import { read, readOrWatch, write } from '@jsxcad/sys';
 
+import Accordion from 'react-bootstrap/Accordion';
 import ControlNote from './ControlNote.js';
 import DownloadNote from './DownloadNote.js';
 import EditNote from './EditNote.js';
 import ErrorNote from './ErrorNote.js';
+import IconNote from './IconNote.js';
 import MdNote from './MdNote.js';
 import React from 'react';
 import { SpinnerCircularSplit } from 'spinners-react';
@@ -140,174 +142,226 @@ export class Notebook extends React.PureComponent {
   }
 
   render() {
-    const {
-      notebookPath,
-      notes,
-      onChange,
-      onClickView,
-      onKeyDown,
-      // selectedLine,
-      state = 'idle',
-      workspace,
-    } = this.props;
-    const ordered = Object.values(notes);
-    const getLine = (note) => {
-      if (note.sourceLocation) {
-        return note.sourceLocation.line;
-      } else {
-        return 0;
-      }
-    };
-    const getNth = (note) => {
-      if (note.sourceLocation) {
-        return note.sourceLocation.nth;
-      } else {
-        return 0;
-      }
-    };
-    const order = (a, b) => {
-      const lineA = getLine(a);
-      const lineB = getLine(b);
-      if (lineA !== lineB) {
-        return lineA - lineB;
-      }
-      const nthA = getNth(a);
-      const nthB = getNth(b);
-      return nthA - nthB;
-    };
-    ordered.sort(order);
-    let line = 0;
-    let id;
-    let hasStub = false;
-    const ids = [];
-    let children;
-    for (const note of ordered) {
-      if (note.sourceLocation.id !== id) {
-        id = note.sourceLocation.id;
-        children = [];
-        ids.push({ id, children });
-      }
-      if (note.hash === 'stub') {
-        hasStub = true;
-      }
-      // FIX: This seems wasteful.
-      const selected = false;
-      let child;
-      if (note.sourceLocation) {
-        line = note.sourceLocation.line;
-      }
-      if (note.view) {
-        child = (
-          <ViewNote
-            key={note.hash}
-            note={note}
-            onClickView={onClickView}
-            selected={selected}
-          />
-        );
-      } else if (note.error) {
-        child = (
-          <ErrorNote
-            key={note.hash}
-            note={note}
-            selected={selected}
-            workspace={workspace}
-          />
-        );
-      } else if (note.md) {
-        child = (
-          <MdNote
-            key={note.hash}
-            note={note}
-            selected={selected}
-            workspace={workspace}
-          />
-        );
-      } else if (note.download) {
-        child = (
-          <DownloadNote
-            key={note.hash}
-            note={note}
-            selected={selected}
-            workspace={workspace}
-          />
-        );
-      } else if (note.control) {
-        child = (
-          <ControlNote
-            key={note.hash}
-            note={note}
-            selected={selected}
-            workspace={workspace}
-          />
-        );
-      } else if (note.sourceText !== undefined) {
-        child = (
-          <EditNote
-            key={note.hash}
-            note={note}
-            onChange={(sourceText) => onChange(note, { sourceText })}
-            onKeyDown={onKeyDown}
-            selected={selected}
-            workspace={workspace}
-          />
-        );
-      }
-      if (children && child) {
-        children.push(child);
-      }
-    }
-
-    if (!hasStub) {
-      // Append an empty editor.
-      const stub = {
-        hash: 'stub',
-        sourceText: '',
-        sourceLocation: { path: notebookPath, line: line + 1 },
+    try {
+      const {
+        notebookPath,
+        notes,
+        onChange,
+        onClickView,
+        onKeyDown,
+        // selectedLine,
+        state = 'idle',
+        workspace,
+      } = this.props;
+      const ordered = Object.values(notes);
+      const getLine = (note) => {
+        if (note.sourceLocation) {
+          return note.sourceLocation.line;
+        } else {
+          return 0;
+        }
       };
-      ids.push({
-        id: '+',
-        children: [
-          <EditNote
-            key={stub.hash}
-            note={stub}
-            onChange={(sourceText) => onChange(stub, { sourceText })}
-            workspace={workspace}
-          />,
-        ],
-      });
-    }
+      const getNth = (note) => {
+        if (note.sourceLocation) {
+          return note.sourceLocation.nth;
+        } else {
+          return 0;
+        }
+      };
+      const order = (a, b) => {
+        const lineA = getLine(a);
+        const lineB = getLine(b);
+        if (lineA !== lineB) {
+          return lineA - lineB;
+        }
+        const nthA = getNth(a);
+        const nthB = getNth(b);
+        return nthA - nthB;
+      };
+      ordered.sort(order);
+      let line = 0;
+      let id;
+      let hasStub = false;
+      const ids = [];
+      let children;
+      let downloads;
+      let errors;
+      let icons;
+      let entry;
+      for (const note of ordered) {
+        if (note.sourceLocation.id !== id) {
+          id = note.sourceLocation.id;
+          children = [];
+          downloads = [];
+          errors = [];
+          icons = [];
+          entry = { id, blue: false, children, downloads, errors, icons };
+          ids.push(entry);
+        }
+        if (note.hash === 'stub') {
+          hasStub = true;
+        }
+        // FIX: This seems wasteful.
+        const selected = false;
+        let child;
+        let download;
+        let error;
+        let icon;
+        if (note.sourceLocation) {
+          line = note.sourceLocation.line;
+        }
+        if (note.view) {
+          child = (
+            <ViewNote
+              key={note.hash}
+              note={note}
+              onClickView={onClickView}
+              selected={selected}
+            />
+          );
+          icon = <IconNote key={note.hash} note={note} />;
+          if (note.blur) {
+            entry.blur = true;
+          }
+        } else if (note.error) {
+          child = (
+            <ErrorNote
+              key={note.hash}
+              note={note}
+              selected={selected}
+              workspace={workspace}
+            />
+          );
+          error = child;
+        } else if (note.md) {
+          child = (
+            <MdNote
+              key={note.hash}
+              note={note}
+              selected={selected}
+              workspace={workspace}
+            />
+          );
+        } else if (note.download) {
+          child = (
+            <DownloadNote
+              key={note.hash}
+              note={note}
+              selected={selected}
+              workspace={workspace}
+            />
+          );
+          download = child;
+        } else if (note.control) {
+          child = (
+            <ControlNote
+              key={note.hash}
+              note={note}
+              selected={selected}
+              workspace={workspace}
+            />
+          );
+        } else if (note.sourceText !== undefined) {
+          child = (
+            <EditNote
+              key={note.hash}
+              note={note}
+              onChange={(sourceText) => onChange(note, { sourceText })}
+              onKeyDown={onKeyDown}
+              selected={selected}
+              workspace={workspace}
+            />
+          );
+        }
+        if (children && child) {
+          children.push(child);
+        }
+        if (downloads && download) {
+          downloads.push(download);
+        }
+        if (icons && icon) {
+          icons.push(icon);
+        }
+        if (errors && error) {
+          errors.push(error);
+        }
+      }
 
-    useEffect(() => mermaid.init(undefined, '.mermaid'));
+      if (!hasStub) {
+        // Append an empty editor.
+        const stub = {
+          hash: 'stub',
+          sourceText: '',
+          sourceLocation: { path: notebookPath, line: line + 1 },
+        };
+        ids.push({
+          id: '+',
+          children: [
+            <EditNote
+              key={stub.hash}
+              note={stub}
+              onChange={(sourceText) => onChange(stub, { sourceText })}
+              workspace={workspace}
+            />,
+          ],
+        });
+      }
 
-    const sections = [];
-    for (const { id, children } of ids) {
-      sections.push(
-        <div>
-          <h3>{id}</h3>
-          <br />
-          {children}
-          <hr />
+      useEffect(() => mermaid.init(undefined, '.mermaid'));
+
+      const sections = [];
+      const compare = (a, b) => {
+        if (a < b) {
+          return -1;
+        } else if (a > b) {
+          return 1;
+        } else {
+          return 0;
+        }
+      };
+      ids.sort((a, b) => compare(a.id, b.id));
+      for (const {
+        id,
+        blur = false,
+        children = [],
+        downloads = [],
+        errors = [],
+        icons = [],
+      } of ids) {
+        sections.push(
+          <Accordion key={id}>
+            <Accordion.Item eventKey={id}>
+              <Accordion.Header>
+                {id} &nbsp;&nbsp; {errors.length > 0 ? '! &nbsp;&nbsp;' : ''}{' '}
+                {icons} &nbsp;&nbsp; {downloads}{' '}
+                {blur ? <SpinnerCircularSplit color="#36d7b7" size="64" /> : ''}
+              </Accordion.Header>
+              <Accordion.Body>{children}</Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        );
+      }
+
+      return (
+        <div
+          id={notebookPath}
+          classList="notebook notes"
+          style={{ overflow: 'auto' }}
+        >
+          {state === 'running' && (
+            <SpinnerCircularSplit
+              color="#36d7b7"
+              size={64}
+              style={{ position: 'fixed', right: 32, top: 64, zIndex: 1000 }}
+            />
+          )}
+          {sections}
         </div>
       );
+    } catch (error) {
+      console.log(error.stack);
+      throw error;
     }
-
-    return (
-      <div
-        id={notebookPath}
-        classList="notebook notes"
-        style={{ overflow: 'auto' }}
-      >
-        {state === 'running' && (
-          <SpinnerCircularSplit
-            color="#36d7b7"
-            size={64}
-            style={{ position: 'fixed', right: 32, top: 64, zIndex: 1000 }}
-          />
-        )}
-        {sections}
-      </div>
-    );
   }
 }
 
