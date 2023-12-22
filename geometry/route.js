17f8b92e1679bd68503d9249f7f9ca8fcef6e4c0
Brian Spilsbury <brian.spilsbury@gmail.com>
2023-12-22 09:23:31 +0900

    Missing files

---
+++ geometry/route.js
@@ -0,0 +1,26 @@
+import { isNotTypeGhost, isNotTypeVoid } from './tagged/type.js';
+
+import { Group } from './Group.js';
+import { linearize } from './tagged/linearize.js';
+import { route as routeWithCgal } from '@jsxcad/algorithm-cgal';
+
+const toolFilter = (geometry) =>
+  ['graph', 'polygonsWithHoles'].includes(geometry.type) &&
+  isNotTypeGhost(geometry) &&
+  isNotTypeVoid(geometry);
+
+const segmentsFilter = (geometry) =>
+  ['segments'].includes(geometry.type) &&
+  isNotTypeGhost(geometry) &&
+  isNotTypeVoid(geometry);
+
+export const Route = (tool, geometries) => {
+  const inputs = [];
+  linearize(tool, toolFilter, inputs);
+  const toolCount = inputs.length;
+  for (const geometry of geometries) {
+    linearize(geometry, segmentsFilter, inputs);
+  }
+  const outputs = routeWithCgal(inputs, toolCount);
+  return Group(outputs);
+};
