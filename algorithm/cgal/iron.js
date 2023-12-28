fc585d2452ced099027f2f3f0ed9078ff9a16d71
Brian Spilsbury <brian.spilsbury@gmail.com>
2023-12-28 09:52:34 +0900

    Missing file

---
+++ algorithm/cgal/iron.js
@@ -0,0 +1,17 @@
+import { STATUS_OK, STATUS_ZERO_THICKNESS } from './status.js';
+import { fromCgalGeometry, withCgalGeometry } from './cgalGeometry.js';
+
+import { ErrorZeroThickness } from './error.js';
+
+export const iron = (inputs, turn = 1 / 360) =>
+  withCgalGeometry('iron', inputs, (cgalGeometry, g) => {
+    const status = g.Iron(cgalGeometry, turn);
+    switch (status) {
+      case STATUS_ZERO_THICKNESS:
+        throw new ErrorZeroThickness('Zero thickness produced by iron');
+      case STATUS_OK:
+        return fromCgalGeometry(cgalGeometry, inputs);
+      default:
+        throw new Error(`Unexpected status ${status}`);
+    }
+  });
