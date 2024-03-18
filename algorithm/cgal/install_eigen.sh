f32779a1fac92fb2e2bb2eb4451c2b26d1da4c8a
Brian Spilsbury <brian.spilsbury@gmail.com>
2024-03-18 18:20:48 +0900

    ignore tar exit code

--- algorithm/cgal/install_eigen.sh
+++ algorithm/cgal/install_eigen.sh
@@ -2,5 +2,5 @@
  rm -f Eigen &&
  wget https://gitlab.com/libeigen/eigen/-/archive/3.3.9/eigen-3.3.9.tar.gz &&
  gunzip eigen-3.3.9.tar.gz &&
- tar xvf eigen-3.3.9.tar &&
+ tar xvf eigen-3.3.9.tar;
  mv eigen-3.3.9/Eigen .)
