4c1ba60e50a1c9dee3e47c9e3e0f4b6c5e3f88d5
Brian Spilsbury <brian.spilsbury@gmail.com>
2024-03-18 18:42:18 +0900

    change gmp url

--- algorithm/cgal/install_gmp.sh
+++ algorithm/cgal/install_gmp.sh
@@ -1,6 +1,7 @@
 (rm -rf gmp-6.2.1 &&
  # . emsdk/emsdk_env.sh &&
- wget https://gmplib.org/download/gmp/gmp-6.2.1.tar.lz &&
+ # wget https://gmplib.org/download/gmp/gmp-6.2.1.tar.lz &&
+ wget https://ftp.gnu.org/gnu/gmp/gmp-6.2.1.tar.lz &&
  tar xf gmp-6.2.1.tar.lz &&
  cd gmp-6.2.1 &&
  # emconfigure ./configure --disable-assembly --host none --enable-cxx --prefix=${HOME}/opt &&
