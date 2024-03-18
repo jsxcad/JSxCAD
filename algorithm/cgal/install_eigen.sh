ab3a641cd07c2c8fc13d39c9caced5600c7c4e31
Brian Spilsbury <brian.spilsbury@gmail.com>
2024-03-18 17:40:32 +0900

    Remove ln -s

--- algorithm/cgal/install_eigen.sh
+++ algorithm/cgal/install_eigen.sh
@@ -2,4 +2,4 @@
  rm -f Eigen &&
  wget https://gitlab.com/libeigen/eigen/-/archive/3.3.9/eigen-3.3.9.tar.gz &&
  tar xzvf eigen-3.3.9.tar.gz &&
- ln -s eigen-3.3.9/Eigen Eigen)
+ mv eigen-3.3.9/Eigen .)
