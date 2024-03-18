a391e12417350c993f4c962aeaf864e891ec22ef
Brian Spilsbury <brian.spilsbury@gmail.com>
2024-03-18 18:27:38 +0900

    Remove emscripten from ci scripts

--- algorithm/cgal/install_glpk.sh
+++ algorithm/cgal/install_glpk.sh
@@ -1,5 +1,5 @@
 (rm -rf glpk-5.0 &&
- . emsdk/emsdk_env.sh &&
+ # . emsdk/emsdk_env.sh &&
  wget http://ftp.gnu.org/gnu/glpk/glpk-5.0.tar.gz &&
  tar xzvf glpk-5.0.tar.gz &&
  cd glpk-5.0 &&
--- algorithm/cgal/install_gmp.sh
+++ algorithm/cgal/install_gmp.sh
@@ -1,9 +1,10 @@
 (rm -rf gmp-6.2.1 &&
- . emsdk/emsdk_env.sh &&
+ # . emsdk/emsdk_env.sh &&
  wget https://gmplib.org/download/gmp/gmp-6.2.1.tar.lz &&
  tar xf gmp-6.2.1.tar.lz &&
  cd gmp-6.2.1 &&
- emconfigure ./configure --disable-assembly --host none --enable-cxx --prefix=${HOME}/opt &&
+ # emconfigure ./configure --disable-assembly --host none --enable-cxx --prefix=${HOME}/opt &&
+ ./configure --disable-assembly --host none --enable-cxx --prefix=${HOME}/opt &&
  make &&
  make install &&
  cd ..)
