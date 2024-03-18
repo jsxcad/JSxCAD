62fc2ca752fd2d86dcaaf6b05829e7a6b6963bef
Brian Spilsbury <brian.spilsbury@gmail.com>
2024-03-18 22:31:29 +0900

    .

--- algorithm/cgal/install_gmp.sh
+++ algorithm/cgal/install_gmp.sh
@@ -1,8 +1,8 @@
 (rm -rf gmp-6.2.1 &&
  # . emsdk/emsdk_env.sh &&
  # wget https://gmplib.org/download/gmp/gmp-6.2.1.tar.lz &&
- wget https://ftp.gnu.org/gnu/gmp/gmp-6.2.1.tar.gz &&
- tar xzf gmp-6.2.1.tar.xz &&
+ wget https://ftp.gnu.org/gnu/gmp/gmp-6.2.1.tar.xz &&
+ tar xf gmp-6.2.1.tar.xz &&
  cd gmp-6.2.1 &&
  # emconfigure ./configure --disable-assembly --host none --enable-cxx --prefix=${HOME}/opt &&
  ./configure --disable-assembly --host none --enable-cxx --prefix=${HOME}/opt &&
