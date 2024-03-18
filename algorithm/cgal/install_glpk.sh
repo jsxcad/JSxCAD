2996264f1a7210607fee153303ec8f686a5658fc
Brian Spilsbury <brian.spilsbury@gmail.com>
2024-03-18 18:28:45 +0900

    more

--- algorithm/cgal/install_glpk.sh
+++ algorithm/cgal/install_glpk.sh
@@ -3,7 +3,8 @@
  wget http://ftp.gnu.org/gnu/glpk/glpk-5.0.tar.gz &&
  tar xzvf glpk-5.0.tar.gz &&
  cd glpk-5.0 &&
- emconfigure ./configure --disable-assembly --host none --enable-cxx --prefix=${HOME}/opt &&
+ # emconfigure ./configure --disable-assembly --host none --enable-cxx --prefix=${HOME}/opt &&
+ ./configure --disable-assembly --host none --enable-cxx --prefix=${HOME}/opt &&
  make &&
  make install &&
  cd ..)
