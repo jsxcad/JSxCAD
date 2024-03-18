cfd4872bd6b619c9f7190d023990529e5bae2b8b
Brian Spilsbury <brian.spilsbury@gmail.com>
2024-03-18 22:42:40 +0900

    .

--- algorithm/cgal/install_mpfr.sh
+++ algorithm/cgal/install_mpfr.sh
@@ -1,9 +1,7 @@
 (# . emsdk/emsdk_env.sh &&
- wget https://www.mpfr.org/mpfr-current/mpfr-4.1.0.tar.xz &&
- wget https://www.mpfr.org/mpfr-current/allpatches &&
- tar xf mpfr-4.1.0.tar.xz &&
- cd mpfr-4.1.0 &&
- patch -N -Z -p1 < ../allpatches  &&
+ wget https://ftp.gnu.org/gnu/mpfr/mpfr-4.2.1.tar.xz &&
+ tar xf mpfr-4.2.1.tar.xz &&
+ cd mpfr-4.2.1 &&
  # emconfigure ./configure --host none --prefix=${HOME}/opt --with-gmp=${HOME}/opt &&
  ./configure --host none --prefix=${HOME}/opt --with-gmp=${HOME}/opt &&
  make &&
