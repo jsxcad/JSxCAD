49e1f98141f07cd571b99b746e6023c25d57567f
Brian Spilsbury <brian.spilsbury@gmail.com>
2024-12-04 11:27:09 +0900

    increase pixel ratio

--- sys/document.js
+++ sys/document.js
@@ -7,6 +7,6 @@ import { onBoot } from './boot.js';
 onBoot(() => {
   const dom = new JSDOM('<html><body></body></html>');
   globalThis.window = dom.window;
-  globalThis.window.devicePixelRatio = 1;
+  globalThis.window.devicePixelRatio = 3;
   globalThis.document = window.document;
 });
